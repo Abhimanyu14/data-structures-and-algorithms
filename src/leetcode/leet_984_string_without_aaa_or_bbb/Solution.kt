7fd31c21d67967ad43a69a785f4042f042171da1
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-07-19 22:44:21 +0530

    leetcode

--- src/leetcode/leet_984_string_without_aaa_or_bbb/Solution.kt
+++ src/leetcode/leet_984_string_without_aaa_or_bbb/Solution.kt
@@ -1,6 +1,6 @@
 package leetcode.leet_984_string_without_aaa_or_bbb
 
-fun solution(a: Int, b: Int, res: String): String? {
+fun recursiveSolution(a: Int, b: Int, res: String): String? {
     println("$a $b $res")
     if (a < 0 || b < 0 || res.length >= 3 && (res.substring(res.length - 3) == "aaa" || res.substring(res.length - 3) == "bbb")) {
         return null
@@ -8,13 +8,46 @@ fun solution(a: Int, b: Int, res: String): String? {
     if (a == 0 && b == 0) {
         return res
     }
-    return solution(a - 1, b, res + "a") ?: solution(a, b - 1, res + "b")
+    return recursiveSolution(a - 1, b, res + "a") ?: recursiveSolution(a, b - 1, res + "b")
+}
+
+
+fun recursiveSolutionDriver(a: Int, b: Int): String {
+    return recursiveSolution(a, b, "") ?: "$a $b not possible"
 }
 
 fun strWithout3a3b(a: Int, b: Int): String {
-    return solution(a, b, "") ?: "$a $b not possible"
+    var result = ""
+    val highOccurrence: Char
+    val lowOccurrence: Char
+    var highOccurrenceCount: Int
+    var lowOccurrenceCount: Int
+    if (a >= b) {
+        highOccurrence = 'a'
+        lowOccurrence = 'b'
+        highOccurrenceCount = a
+        lowOccurrenceCount = b
+    } else {
+        highOccurrence = 'b'
+        lowOccurrence = 'a'
+        highOccurrenceCount = b
+        lowOccurrenceCount = a
+    }
+    val diff = if (highOccurrenceCount <= lowOccurrenceCount * 2) {
+        highOccurrenceCount - lowOccurrenceCount
+    } else {
+        lowOccurrenceCount
+    }
+    result += "$highOccurrence$highOccurrence$lowOccurrence".repeat(diff)
+    highOccurrenceCount -= diff * 2
+    lowOccurrenceCount -= diff
+    result += "$highOccurrence$lowOccurrence".repeat(lowOccurrenceCount)
+    highOccurrenceCount -= lowOccurrenceCount
+    lowOccurrenceCount -= lowOccurrenceCount
+    result += highOccurrence.toString().repeat(highOccurrenceCount)
+    return result
 }
 
 fun main() {
-    println(strWithout3a3b(1, 3))
+    println(strWithout3a3b(4, 1))
 }
