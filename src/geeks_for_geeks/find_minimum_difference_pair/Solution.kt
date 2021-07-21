76aafa241704572fc89e61beef9b2aa249ef74e4
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-07-21 15:42:39 +0530

    leetcode

---
+++ src/geeks_for_geeks/find_minimum_difference_pair/Solution.kt
@@ -0,0 +1,35 @@
+package geeks_for_geeks.find_minimum_difference_pair
+
+import kotlin.math.abs
+
+fun bruteForceSolution(arr: IntArray): Int {
+    var result = Int.MAX_VALUE
+    for (i in arr.indices) {
+        for (j in i + 1 until arr.size) {
+            result = minOf(abs(arr[i] - arr[j]), result)
+        }
+    }
+    return result
+}
+
+fun solutionUsingSorting(arr: IntArray): Int {
+    val sorted = arr.sortedArray()
+    var result = abs(sorted[0] - sorted[1])
+    for (i in 2 until sorted.size) {
+        result = minOf(result, abs(sorted[i] - sorted[i - 1]))
+    }
+    return result
+}
+
+fun main() {
+    val input1 = intArrayOf(1, 5, 3, 19, 18, 25)
+    val input2 = intArrayOf(30, 5, 20, 9)
+    val input3 = intArrayOf(1, 19, -4, 31, 38, 25, 100)
+    println(bruteForceSolution(input1))
+    println(bruteForceSolution(input2))
+    println(bruteForceSolution(input3))
+    println()
+    println(solutionUsingSorting(input1))
+    println(solutionUsingSorting(input2))
+    println(solutionUsingSorting(input3))
+}
