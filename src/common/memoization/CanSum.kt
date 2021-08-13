0c8a197215e45323038b29b4dc1bd0ffe1eac239
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-08-13 18:25:03 +0530

    memoization

---
+++ src/common/memoization/CanSum.kt
@@ -0,0 +1,51 @@
+package common.memoization
+
+/**
+ * Question - https://youtu.be/oBt53YbR9Kk
+ * Check if given sum can be obtained from the set of numbers provided and they can be repeated
+ * Result: Return a boolean denoting possible or not
+ */
+fun canSumRecursive(nums: IntArray, sum: Int): Boolean {
+    if (sum == 0) {
+        return true
+    }
+    if (sum < 0) {
+        return false
+    }
+    return nums.any {
+        canSumRecursive(nums, sum - it)
+    }
+}
+
+/**
+ * Memoization
+ */
+fun canSum(nums: IntArray, sum: Int, memo: HashMap<Int, Boolean>): Boolean {
+    memo[sum]?.let {
+        return it
+    }
+    if (sum == 0) {
+        return true
+    }
+    if (sum < 0) {
+        return false
+    }
+    memo[sum] = nums.any {
+        canSum(nums, sum - it, memo)
+    }
+    return memo[sum]!!
+}
+
+fun main() {
+    val input1 = intArrayOf(2, 3)
+    val input2 = intArrayOf(5, 3, 4, 7)
+    val input3 = intArrayOf(2, 4)
+    val input4 = intArrayOf(2, 3, 5)
+    val input5 = intArrayOf(7, 14)
+
+    println(canSum(input1, 7, hashMapOf())) // true
+    println(canSum(input2, 7, hashMapOf())) // true
+    println(canSum(input3, 7, hashMapOf())) // false
+    println(canSum(input4, 8, hashMapOf())) // true
+    println(canSum(input5, 300, hashMapOf())) // false
+}
---
+++ src/common/memoization/CountConstruct.kt
@@ -0,0 +1,33 @@
+package common.memoization
+
+/**
+ * Question - https://youtu.be/oBt53YbR9Kk
+ * Check if given string can be obtained from the set of words provided and they can be repeated
+ * Result: Return a count of number of possible ways
+ */
+fun countConstruct(words: List<String>, target: String, memo: HashMap<String, Int>): Int {
+    if (target == "") {
+        return 1
+    }
+    memo[target]?.let {
+        return it
+    }
+    memo[target] = 0
+    for (i in words.indices) {
+        if (target.startsWith(words[i])) {
+            countConstruct(words, target.removePrefix(words[i]), memo).let {
+                memo[target] = memo[target]?.plus(it) ?: 0
+            }
+        }
+    }
+    return memo[target] ?: 0
+}
+
+fun main() {
+    val input1 = listOf("ab", "abc", "cd", "def", "abcd") // 1
+    val input2 = listOf("bo", "rd", "ate", "t", "ska", "sk", "boar") // 0
+    val input3 = listOf("purp", "p", "ur", "le", "purpl") //
+    println(countConstruct(input1, "abcdef", hashMapOf()))
+    println(countConstruct(input2, "skateboard", hashMapOf()))
+    println(countConstruct(input3, "purple", hashMapOf()))
+}
