82ecb75595a06fa3c8a7d7a978c0200f25196b4d
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-09-12 21:46:54 +0530

    tail recursion

--- src/common/recursion/Fibonacci.kt
+++ src/common/recursion/Fibonacci.kt
@@ -1,14 +1,43 @@
 package common.recursion
 
-fun fibonacci(n: Int): Int {
+fun fibonacciTailRecursion(first: Int, second: Int, n: Int): Int {
+    if (n == 0) {
+        return first + second
+    }
+    return fibonacciTailRecursion(second, first + second, n - 1)
+}
+
+fun fibonacciTailRecursionDriver(n: Int): Int {
+    return if (n == 0 || n == 1) {
+        n
+    } else fibonacciTailRecursion(0, 1, n - 2)
+}
+
+fun fibonacciRecursive(n: Int): Int {
     return if (n == 0 || n == 1) {
         n
     } else {
-        fibonacci(n - 1) + fibonacci(n - 2)
+        fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
     }
 }
 
+fun fibonacciIterative(n: Int): Int {
+    if (n == 0 || n == 1) {
+        return n
+    }
+    var first = 0
+    var second = 1
+    for (i in 2 until n) {
+        second += first
+        first = second - first
+    }
+    return first + second
+}
+
 fun main() {
-    val result = fibonacci(5)
-    print(result)
+    IntArray(10) { it }.forEach {
+        // println(fibonacci(it))
+        // println(fibonacciIterative(it))
+        println(fibonacciTailRecursionDriver(it))
+    }
 }
