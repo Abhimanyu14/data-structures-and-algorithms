ef3c4c36877a063292ec4618316a911de360a31e
Abhimanyu14 <abhimanyu.n14@gmail.com>
2022-07-14 22:02:58 +0530

    leetcode

--- src/leetcode/leet_1_two_sum/Solution.kt
+++ src/leetcode/leet_1_two_sum/Solution.kt
@@ -1,17 +1,63 @@
 package leetcode.leet_1_two_sum
 
-fun twoSum(m: Int, arr: Array<Int>): Array<Int> {
-    val map = hashMapOf<Int, Int>()
-    for (i in arr.indices) {
-        if (map.containsKey(m - arr[i])) {
-            return arrayOf(map[m - arr[i]] ?: -1, i)
+/**
+ * Using loop
+ *
+ * Stats
+ * Runtime: 279 ms, faster than 87.54%
+ * Memory Usage: 38.9 MB, less than 85.06%
+ */
+private fun twoSumUsingLoop(nums: IntArray, target: Int): IntArray {
+    for (i in nums.indices) {
+        for (j in i + 1..nums.lastIndex) {
+            if (nums[i] + nums[j] == target) {
+                return intArrayOf(i, j)
+            }
         }
-        map[arr[i]] = i
     }
-    return emptyArray()
+    return intArrayOf()
+}
+
+/**
+ * Using map
+ *
+ * Stats
+ * Runtime: 218 ms, faster than 96.60%
+ * Memory Usage: 38.2 MB, less than 87.41%
+ */
+private fun twoSumUsingMap(nums: IntArray, target: Int): IntArray {
+    val map = hashMapOf<Int, Int>()
+    nums.forEachIndexed { index, num ->
+        map[target - num]?.let {
+            return intArrayOf(it, index)
+        }
+        map[num] = index
+    }
+    return intArrayOf()
+}
+
+/**
+ * Using diff map
+ *
+ * Stats
+ * Runtime: 218 ms, faster than 96.60%
+ * Memory Usage: 38.2 MB, less than 87.41%
+ */
+private fun twoSum(nums: IntArray, target: Int): IntArray {
+    val diffMap = hashMapOf<Int, Int>()
+    nums.forEachIndexed { index, num ->
+        diffMap[num]?.let {
+            return intArrayOf(it, index)
+        }
+        diffMap[target - num] = index
+    }
+    return intArrayOf()
 }
 
 fun main() {
-    println(twoSum(4, arrayOf(1, 4, 5, 3, 2)).joinToString())
-    println(twoSum(4, arrayOf(2, 2, 4, 3)).joinToString())
+    println(twoSum(intArrayOf(2, 7, 11, 15), 9).joinToString())
+    println(twoSum(intArrayOf(3, 2, 4), 6).joinToString())
+    println(twoSum(intArrayOf(3, 3), 6).joinToString())
+    println(twoSum(intArrayOf(1, 4, 5, 3, 2), 4).joinToString())
+    println(twoSum(intArrayOf(2, 2, 4, 3), 5).joinToString())
 }
