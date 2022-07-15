d9971c370131c824f032f4e863417d0e0b8c0f0b
Abhimanyu14 <abhimanyu.n14@gmail.com>
2022-07-15 13:31:09 +0530

    leetcode

--- src/common/data_structures/TreeNode.kt
+++ src/common/data_structures/TreeNode.kt
@@ -4,3 +4,8 @@ class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
 }
+
+fun TreeNode?.height(height: Int = -1): Int {
+    if (this == null) return height
+    return maxOf(left.height(height + 1), right.height(height + 1))
+}
