e5ed7a91cafe40edc15e035ecf34924ee3b3bcfa
Abhimanyu14 <abhimanyu.n14@gmail.com>
2022-08-01 08:41:26 +0530

    leetcode

---
+++ src/leetcode/leet_35_search_insert_position/Solution.kt
@@ -0,0 +1,31 @@
+package leetcode.leet_35_search_insert_position
+
+/**
+ * Using binary search, loops
+ *
+ * Stats
+ * Runtime: 192 ms, faster than 94.78%
+ * Memory Usage: 37.7 MB, less than 73.79%
+ */
+private fun searchInsert(nums: IntArray, target: Int): Int {
+    var low = 0
+    var high = nums.lastIndex
+    while (low <= high) {
+        val mid = ((high + low) / 2)
+        if (nums[mid] == target) {
+            return mid
+        }
+        if (target > nums[mid]) {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return low
+}
+
+fun main() {
+    println(searchInsert(intArrayOf(1, 3, 5, 6), 5))
+    println(searchInsert(intArrayOf(1, 3, 5, 6), 2))
+    println(searchInsert(intArrayOf(1, 3, 5, 6), 7))
+}
