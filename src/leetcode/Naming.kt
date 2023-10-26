f0ba7504c1b58dd6a3d9c72ae5b269f4cd352456
Abhimanyu14 <abhimanyu.n14@gmail.com>
2023-10-26 13:30:38 +0530

    leetcode

--- src/leetcode/Naming.kt
+++ src/leetcode/Naming.kt
@@ -1,7 +1,7 @@
 package leetcode
 
 private fun main() {
-    println(formatLeetcodeTitle("515. Find Largest Value in Each Tree Row"))
+    println(formatLeetcodeTitle("823. Binary Trees With Factors"))
     println(formatLeetcodeTitle("1081. Smallest Subsequence of Distinct Characters"))
 }
 
