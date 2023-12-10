b8c5549478711318c0839fcdeb0fcfe6a4a34a9e
Abhimanyu14 <abhimanyu.n14@gmail.com>
2023-12-10 14:08:19 +0530

    leetcode

--- src/leetcode/leet_1600_to_1699/leet_1669_merge_in_between_linked_lists/Solution.kt
+++ src/leetcode/leet_1600_to_1699/leet_1669_merge_in_between_linked_lists/Solution.kt
@@ -3,45 +3,34 @@ package leetcode.leet_1600_to_1699.leet_1669_merge_in_between_linked_lists
 import data_structures_and_algorithms.linkedlist.ListNode
 
 /**
- * WIP
+ * leetcode - https://leetcode.com/problems/merge-in-between-linked-lists/
+ *
+ * Using iteration
  *
  * Stats
- *
+ * Runtime: 435 ms, faster than 100.00%
+ * Memory Usage: 42.8 MB, less than 33.33%
  */
 private fun mergeInBetween(list1: ListNode?, a: Int, b: Int, list2: ListNode?): ListNode? {
-    // The indices are a bit confusing, refer to the question example to understand the indexing logic
-
-    // Counter
-    var counter = 0
-
-    var aPos = list1
-    var current = list1?.next
-
-    // Find list2 tail node
-    var list2Tail = list2
+    val dummy = ListNode(-1).apply {
+        next = list1
+    }
+    var prev: ListNode? = dummy
+    var current: ListNode? = list1
+    repeat(a) {
+        prev = current
+        current = current?.next
+    }
+    prev?.next = list2
+    var list2Tail: ListNode? = list2
     while (list2Tail?.next != null) {
-        list2Tail = list2Tail?.next
+        list2Tail = list2Tail.next
     }
-
-    // Iterate till the node to which we want to attach list 2
-    while (counter < b + 1) {
-        if (counter == a - 1) {
-            // Attach list 2 to the 'a' th node
-            aPos?.next = list2
-        } else if (counter < a) {
-            // Increment if we have not reached ath node
-            aPos = aPos?.next
-        }
-        counter++
-        // To skip moving the current node position at the end of the loop
-        if (counter < b + 1) {
-            current = current?.next
-        }
+    repeat(b - a) {
+        current = current?.next
     }
-
-    // Attach list 2 node tail to the current position in list 1
-    list2Tail?.next = current
-    return list1
+    list2Tail?.next = current?.next
+    return dummy.next
 }
 
 private fun main() {
