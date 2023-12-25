d19186478c5d1fe6c94f7a48ba4b4c93ec88cb83
Abhimanyu14 <abhimanyu.n14@gmail.com>
2023-12-25 07:26:04 +0530

    leetcode

--- src/leetcode/leet_0_to_99/leet_91_decode_ways/Solution.kt
+++ src/leetcode/leet_0_to_99/leet_91_decode_ways/Solution.kt
@@ -3,37 +3,46 @@ package leetcode.leet_0_to_99.leet_91_decode_ways
 /**
  * leetcode - https://leetcode.com/problems/decode-ways/
  *
- * TODO(Abhi) - To complete
- *
- * Using
+ * Using map and recursion
  *
  * Stats
- *
+ * Runtime: 156 ms, faster than 51.16%
+ * Memory Usage: 35.6 MB, less than 38.76%
  */
 private fun numDecodings(s: String): Int {
-    val memo = mutableMapOf<Int, Int>()
-
-    fun numDecodings(s: String, index: Int): Int {
-        if (memo.contains(index)) {
-            return memo[index] ?: 0
-        }
-        if (s.isEmpty()) {
-            return 1
-        }
-        if (s.first() == '0') {
-            return 0
-        }
-        if (index == s.lastIndex) {
-            return 1
-        }
-        var result = numDecodings(s, index + 1)
-        if (s.substring(index, index + 2).toInt() <= 26) {
-            result += numDecodings(s, index + 2)
-        }
-        memo[index] = result
-        return result
+    // leading zeroes
+    if (s[0] == '0') {
+        return 0
     }
-    return numDecodings(s, 0)
+    // string of length 1
+    if (s.length == 1) {
+        return 1
+    }
+    val memo = mutableMapOf<Int, Int>()
+    fun getCount(index: Int): Int {
+        memo[index]?.let {
+            return it
+        }
+        memo[index] = if (s[index] == '0') {
+            // invalid mapping
+            0
+        } else if (index == s.lastIndex) {
+            // string of length 1
+            1
+        } else {
+            if ((s[index] == '1') || (s[index] == '2' && s[index + 1] - '0' <= 6)) {
+                if (s.lastIndex == index + 1) {
+                    getCount(index + 1) + 1
+                } else {
+                    getCount(index + 1) + getCount(index + 2)
+                }
+            } else {
+                getCount(index + 1)
+            }
+        }
+        return memo[index] ?: 0
+    }
+    return getCount(0)
 }
 
 private fun main() {
