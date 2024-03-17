7f89f4239a192722f5183d45a2cf85a96e8ba93d
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-03-17 22:50:23 +0530

    leetcode

--- src/leetcode/leet_800_to_899/leet_875_koko_eating_bananas/Solution.kt
+++ src/leetcode/leet_800_to_899/leet_875_koko_eating_bananas/Solution.kt
@@ -8,11 +8,47 @@ import kotlin.math.max
  *
  * Using binary search
  *
+ * Binary search template - https://leetcode.com/discuss/study-guide/786126/Python-Powerful-Ultimate-Binary-Search-Template.-Solved-many-problems
+ *
+ * Stats
+ * Runtime: 285 ms, faster than 40.36%
+ * Memory Usage: 41 MB, less than 30.04%
+ */
+private fun minEatingSpeed(piles: IntArray, h: Int): Int {
+    if (h < piles.size) {
+        return -1 // Invalid input
+    }
+    if (h == piles.size) {
+        return piles.max()
+    }
+    fun canCompleteEating(n: Int): Boolean {
+        return piles.sumOf { ceil(it.toDouble() / n).toInt() } <= h
+    }
+
+    var left = 1
+    var right = piles.max()
+    while (left < right) {
+        val mid = left + ((right - left) / 2)
+        println("$left $right $mid")
+        if (canCompleteEating(mid)) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/koko-eating-bananas/
+ *
+ * Using binary search
+ *
  * Stats
  * Runtime: 286 ms, faster than 37.04%
  * Memory Usage: 41.7 MB, less than 7.87%
  */
-private fun minEatingSpeed(piles: IntArray, h: Int): Int {
+private fun minEatingSpeedUsingBinarySearch(piles: IntArray, h: Int): Int {
     if (h <= piles.size) {
         return piles.max()
     }
@@ -23,6 +59,7 @@ private fun minEatingSpeed(piles: IntArray, h: Int): Int {
         }
         return result <= h
     }
+
     var pilesMax = 0
     var pilesSum = 0.0
     piles.forEach {
