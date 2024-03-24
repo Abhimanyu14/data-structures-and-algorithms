2640318e776b65237cac80ae65d28f92040ee8c8
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-03-24 12:53:25 +0530

    leetcode

--- src/leetcode/leet_200_to_299/leet_287_find_the_duplicate_number/Solution.kt
+++ src/leetcode/leet_200_to_299/leet_287_find_the_duplicate_number/Solution.kt
@@ -1,5 +1,55 @@
 package leetcode.leet_200_to_299.leet_287_find_the_duplicate_number
 
+import kotlin.math.abs
+
+/**
+ * leetcode - https://leetcode.com/problems/find-the-duplicate-number/
+ *
+ * Using in place manipulation
+ *
+ * Stats
+ * Runtime: 378 ms, faster than 86.87%
+ * Memory Usage: 56.1 MB, less than 80.30%
+ */
+private fun findDuplicate(nums: IntArray): Int {
+    nums.forEach { num ->
+        if (nums[abs(num)] < 0) {
+            return abs(num)
+        } else {
+            nums[abs(num)] = -nums[abs(num)]
+        }
+    }
+    return -1
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/find-the-duplicate-number/
+ *
+ * Using Floyd's cycle detection
+ *
+ * Stats
+ * Runtime: 423 ms, faster than 49.49%
+ * Memory Usage: 58.2 MB, less than 13.64%
+ */
+private fun findDuplicateUsingFloyd(nums: IntArray): Int {
+    var slow = 0
+    var fast = 0
+
+    while(true) {
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if (fast == slow) break
+    }
+
+    var slow2 = 0
+    while(slow != slow2) {
+        slow = nums[slow]
+        slow2 = nums[slow2]
+    }
+
+    return slow
+}
+
 /**
  * leetcode - https://leetcode.com/problems/find-the-duplicate-number/
  *
@@ -9,7 +59,7 @@ package leetcode.leet_200_to_299.leet_287_find_the_duplicate_number
  * Runtime: 406 ms, faster than 64.60%
  * Memory Usage: 57.7 MB, less than 17.26%
  */
-private fun findDuplicate(nums: IntArray): Int {
+private fun findDuplicateUsingArray(nums: IntArray): Int {
     while (nums[0] != nums[nums[0]]) {
         nums[nums[0]] = nums[0].also {
             nums[0] = nums[nums[0]]
