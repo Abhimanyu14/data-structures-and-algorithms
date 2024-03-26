d535d3d3c13a2489933d153bedada627a6ba59b5
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-03-26 08:45:03 +0530

    leetcode

--- src/leetcode/leet_1700_to_1799/leet_1751_maximum_number_of_events_that_can_be_attended_ii/Solution.kt
+++ src/leetcode/leet_1700_to_1799/leet_1751_maximum_number_of_events_that_can_be_attended_ii/Solution.kt
@@ -5,19 +5,22 @@ import kotlin.math.max
 /**
  * leetcode - https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended-ii/
  *
- * TODO(Abhi) - To complete
- *
- * Using sorting - sortWith and compareBy to sort using multiple values
+ * Using sorting - sortWith and compareBy to sort using multiple values, binary search and memoization
  *
  * Stats
+ * Runtime: 1696 ms, faster than 100.00%
+ * Memory Usage: 366.5 MB, less than 100.00%
  *
+ * Time - O(N * K * logN)
+ * Space = O(N * K)
  */
 private fun maxValue(events: Array<IntArray>, k: Int): Int {
     events.sortWith(compareBy({ it[0] }, { it[1] }))
+    val memo: MutableMap<Pair<Int, Int>, Int> = mutableMapOf()
 
     fun nextEvent(pos: Int): Int {
         var left = pos + 1
-        var right = events.lastIndex
+        var right = events.size
         while (left < right) {
             val mid = left + ((right - left) / 2)
             if (events[mid][0] > events[pos][1]) {
@@ -30,12 +33,19 @@ private fun maxValue(events: Array<IntArray>, k: Int): Int {
     }
 
     fun helper(pos: Int, updatedK: Int): Int {
+        memo[Pair(pos, updatedK)]?.let {
+            return it
+        }
+
         if (pos > events.lastIndex || updatedK == 0) {
             return 0
         }
-        val selected = events[pos][2] + helper(nextEvent(pos), k - 1)
-        val notSelected = helper(pos + 1, k)
-        return max(selected, notSelected)
+        val selected = events[pos][2] + helper(nextEvent(pos), updatedK - 1)
+        val notSelected = helper(pos + 1, updatedK)
+        memo[Pair(pos, updatedK)] = max(selected, notSelected)
+        memo.getOrDefault(Pair(pos, updatedK), 0).let {
+            return it
+        }
     }
 
     return helper(0, k)
