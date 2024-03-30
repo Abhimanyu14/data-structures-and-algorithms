82769b8f6d8c1e159b92df5ae8c20aba831f02d4
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-03-31 01:03:53 +0530

    leetcode

---
+++ src/leetcode/leet_500_to_599/leet_584_find_customer_referee/Solution.kt
@@ -0,0 +1,10 @@
+package leetcode.leet_500_to_599.leet_584_find_customer_referee
+
+/**
+ * leetcode - https://leetcode.com/problems/find-customer-referee/
+ *
+ * TODO(Abhi) - SQL question
+ */
+private fun main() {
+
+}
