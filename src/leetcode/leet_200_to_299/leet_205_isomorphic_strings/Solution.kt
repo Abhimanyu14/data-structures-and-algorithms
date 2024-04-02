461b9ebb2c1722368150d41fc3e60b8093d1f9ff
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-04-02 21:31:36 +0530

    leetcode

--- src/leetcode/leet_200_to_299/leet_205_isomorphic_strings/Solution.kt
+++ src/leetcode/leet_200_to_299/leet_205_isomorphic_strings/Solution.kt
@@ -1,16 +1,20 @@
 package leetcode.leet_200_to_299.leet_205_isomorphic_strings
 
 /**
+ * leetcode - https://leetcode.com/problems/isomorphic-strings/
+ *
  * Using map
+ *
+ * Difficulty - Easy
  *
  * Stats
  * Runtime: 211 ms, faster than 94.22%
  * Memory Usage: 35.5 MB, less than 94.22%
+ *
+ * Time -
+ * Space -
  */
 private fun isIsomorphic(s: String, t: String): Boolean {
-    if (s.length != t.length) {
-        return false
-    }
     val map = hashMapOf<Char, Char>()
     s.forEachIndexed { index, c ->
         if (map.containsKey(c) || map.containsValue(t[index])) {
