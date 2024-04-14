895b81afa75423164e7da3b33edb048734e3a387
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-04-14 13:10:47 +0530

    leetcode

--- src/leetcode/Naming.kt
+++ src/leetcode/Naming.kt
@@ -1,7 +1,7 @@
 package leetcode
 
 private fun main() {
-    println(formatLeetcodeTitle("85. Maximal Rectangle"))
+    println(formatLeetcodeTitle("404. Sum of Left Leaves"))
 
     // println(formatHackerrankTitle("Is This a Binary Search Tree?"))
 }
