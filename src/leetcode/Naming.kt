2f8de4cce133a25ba76aae11cfd18744ad7bec52
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-08-07 09:53:05 +0530

    leetcode

--- src/leetcode/Naming.kt
+++ src/leetcode/Naming.kt
@@ -1,7 +1,7 @@
 package leetcode
 
 private fun main() {
-    println(formatLeetcodeTitle("3016. Minimum Number of Pushes to Type Word II"))
+    println(formatLeetcodeTitle("273. Integer to English Words"))
 
     // println(formatHackerrankTitle("Is This a Binary Search Tree?"))
 }
