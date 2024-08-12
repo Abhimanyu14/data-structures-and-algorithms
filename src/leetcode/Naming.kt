13eb71b34786a507be8a9ac77b79a60d05507faa
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-08-12 16:39:25 +0530

    leetcode

--- src/leetcode/Naming.kt
+++ src/leetcode/Naming.kt
@@ -1,7 +1,7 @@
 package leetcode
 
 private fun main() {
-    println(formatLeetcodeTitle("1568. Minimum Number of Days to Disconnect Island"))
+    println(formatLeetcodeTitle("703. Kth Largest Element in a Stream"))
 
     // println(formatHackerrankTitle("Is This a Binary Search Tree?"))
 }
