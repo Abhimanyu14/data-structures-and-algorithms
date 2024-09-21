84a9c2237fab605ccf812ecbb9fdd350909444fb
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-09-21 17:40:32 +0530

    leetcode

---
+++ src/leetcode/leet_200_to_299/leet_241_different_ways_to_add_parentheses/Solution.kt
@@ -0,0 +1,20 @@
+package leetcode.leet_200_to_299.leet_241_different_ways_to_add_parentheses
+
+/**
+ * leetcode - https://leetcode.com/problems/different-ways-to-add-parentheses/
+ *
+ * TODO(Abhi) - To complete
+ *
+ * Using
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ *
+ * Time -
+ * Space -
+ */
+
+private fun main() {
+
+}
---
+++ src/leetcode/leet_300_to_399/leet_386_lexicographical_numbers/Solution.kt
@@ -0,0 +1,44 @@
+package leetcode.leet_300_to_399.leet_386_lexicographical_numbers
+
+
+/**
+ * leetcode - https://leetcode.com/problems/lexicographical-numbers/
+ *
+ * TODO(Abhi) - To complete
+ *
+ * Using
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ *
+ * Time -
+ * Space -
+ */
+
+private fun lexicalOrder(n: Int): List<Int> {
+    val lexicographicalNumbers: MutableList<Int> = ArrayList()
+    var currentNumber = 1
+
+    // Generate numbers from 1 to n
+    for (i in 0..<n) {
+        lexicographicalNumbers.add(currentNumber)
+
+        // If multiplying the current number by 10 is within the limit, do it
+        if (currentNumber * 10 <= n) {
+            currentNumber *= 10
+        } else {
+            // Adjust the current number by moving up one digit
+            while (currentNumber % 10 == 9 || currentNumber >= n) {
+                currentNumber /= 10 // Remove the last digit
+            }
+            currentNumber += 1 // Increment the number
+        }
+    }
+
+    return lexicographicalNumbers
+}
+
+private fun main() {
+
+}
