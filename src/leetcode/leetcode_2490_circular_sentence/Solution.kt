3685b73d35c433c1527844ab5170d40fb1bfcbc4
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-02 07:48:22 +0530

    leetcode

--- src/leetcode/leetcode_2490_circular_sentence/Solution.kt
+++ src/leetcode/leetcode_2490_circular_sentence/Solution.kt
@@ -1,19 +1,28 @@
 package leetcode.leetcode_2490_circular_sentence
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/circular-sentence/?envType=daily-question&envId=2024-11-02
  *
- * TODO(Abhi) - To revisit
+ * Using iteration
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Easy
  *
  * Stats
+ * Runtime: 9 ms, faster than 100.00%
+ * Memory Usage: 37.5 MB, less than 50.00%
  *
  * Time -
  * Space -
  */
+private fun isCircularSentence(sentence: String): Boolean {
+    for (i in 1..<sentence.length) {
+        if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1]) {
+            return false
+        }
+    }
+    return sentence[0] == sentence.last()
+}
+
 private fun main() {
 
 }
