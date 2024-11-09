02108b82d070e57bb76c966a781f1c1e7d26eb6e
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-09 20:15:11 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2824_count_pairs_whose_sum_is_less_than_target/Solution.kt
+++ src/main/java/leetcode/leetcode_2824_count_pairs_whose_sum_is_less_than_target/Solution.kt
@@ -3,17 +3,44 @@ package leetcode.leetcode_2824_count_pairs_whose_sum_is_less_than_target
 /**
  * leetcode - https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
  *
- * TODO(Abhi) - To revisit
- *
- * Using
+ * Using [sorted]
  *
  * Difficulty - Easy
  *
+ * Note:
+ * 1. [List] does not have an in-place sort method
+ *
  * Stats
+ * Runtime: 21 ms, faster than 46.15%
+ * Memory Usage: 38.1 MB, less than 37.50%
  *
  * Time -
  * Space -
  */
+private fun countPairs(nums: List<Int>, target: Int): Int {
+    if (nums.size <= 1) {
+        return 0
+    }
+    var result = 0
+    val sorted = nums.sorted()
+    var first = 0
+    var second = 1
+    while (first < sorted.lastIndex) {
+        while (second < sorted.lastIndex && ((sorted[first] + sorted[second + 1]) < target)) {
+            second++
+        }
+        while (first < second && ((sorted[first] + sorted[second]) >= target)) {
+            second--
+        }
+        if (first == second) {
+            return result
+        }
+        result += (second - first)
+        first++
+    }
+    return result
+}
+
 private fun main() {
 
 }
--- src/main/java/leetcode/leetcode_3211_generate_binary_strings_without_adjacent_zeros/Solution.kt
+++ src/main/java/leetcode/leetcode_3211_generate_binary_strings_without_adjacent_zeros/Solution.kt
@@ -1,19 +1,35 @@
 package leetcode.leetcode_3211_generate_binary_strings_without_adjacent_zeros
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/generate-binary-strings-without-adjacent-zeros/
  *
- * TODO(Abhi) - To revisit
+ * Using recursion
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 220 ms, faster than 50.00%
+ * Memory Usage: 39.3 MB, less than 56.00%
  *
  * Time -
  * Space -
  */
+private fun validStrings(n: Int): List<String> {
+    val result = mutableListOf<String>()
+    fun generate(current: String) {
+        if (current.length == n) {
+            result.add(current)
+            return
+        }
+        generate(current + "1")
+        if (current.isEmpty() || current.last() != '0') {
+            generate(current + "0")
+        }
+    }
+    generate("")
+    return result
+}
+
 private fun main() {
 
 }
