773f8e40a761daf5625b2774ccefa058ed718d23
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-10 10:50:58 +0530

    leetcode

--- src/main/java/leetcode/leetcode_325_maximum_size_subarray_sum_equals_k/Solution.kt
+++ src/main/java/leetcode/leetcode_325_maximum_size_subarray_sum_equals_k/Solution.kt
@@ -1,19 +1,42 @@
 package leetcode.leetcode_325_maximum_size_subarray_sum_equals_k
 
+import kotlin.math.max
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/description/
  *
  * TODO(Abhi) - To revisit
  *
- * Using
+ * Using prefix sum and map
  *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 132 ms, faster than 80.00%
+ * Memory Usage: 70.48 MB, less than 86.67%
  *
  * Time -
  * Space -
  */
+private fun maxSubArrayLen(nums: IntArray, k: Int): Int {
+    var result = 0
+    var prefixSum = 0
+    val map = mutableMapOf<Int, Int>()
+    nums.forEachIndexed { index, num ->
+        prefixSum += num
+        if (prefixSum == k) {
+            result = index + 1
+        }
+        if (map.contains(prefixSum - k)) {
+            result = max(result, index - map.getOrDefault(prefixSum - k, 0))
+        }
+        if (!map.contains(prefixSum)) {
+            map[prefixSum] = index
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
--- src/main/java/leetcode/leetcode_496_next_greater_element_i/Solution.kt
+++ src/main/java/leetcode/leetcode_496_next_greater_element_i/Solution.kt
@@ -1,19 +1,42 @@
 package leetcode.leetcode_496_next_greater_element_i
 
+import java.util.Stack
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/next-greater-element-i/
  *
- * TODO(Abhi) - To revisit
+ * Using monotonic stack
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Easy
  *
  * Stats
+ * Runtime: 22 ms, faster than 37.10%
+ * Memory Usage: 38 MB, less than 41.95%
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(N)
  */
+private fun nextGreaterElement(nums1: IntArray, nums2: IntArray): IntArray {
+    val nextGreaterElement = mutableMapOf<Int, Int>()
+    val stack = Stack<Int>()
+    for (i in (nums2.lastIndex) downTo 0) {
+        while (stack.isNotEmpty() && stack.peek() < nums2[i]) {
+            stack.pop()
+        }
+        if (stack.isEmpty()) {
+            nextGreaterElement[nums2[i]] = -1
+        } else {
+            nextGreaterElement[nums2[i]] = stack.peek()
+        }
+        stack.push(nums2[i])
+    }
+    val result = IntArray(nums1.size)
+    nums1.forEachIndexed { index, num1 ->
+        result[index] = nextGreaterElement.getOrDefault(num1, 0)
+    }
+    return result
+}
+
 private fun main() {
 
 }
