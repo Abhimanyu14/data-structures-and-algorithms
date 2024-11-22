3ec33d38b439415868b061ef6e5b564a97c17a3e
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-22 09:27:44 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1072_flip_columns_for_maximum_number_of_equal_rows/Solution.kt
+++ src/main/java/leetcode/leetcode_1072_flip_columns_for_maximum_number_of_equal_rows/Solution.kt
@@ -1,19 +1,43 @@
 package leetcode.leetcode_1072_flip_columns_for_maximum_number_of_equal_rows
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/flip-columns-for-maximum-number-of-equal-rows/
  *
- * TODO(Abhi) - To revisit
+ * Using StringBuilder and map
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 42 ms, faster than 100.00%
+ * Memory Usage: 52.8 MB, less than 100.00%
  *
  * Time -
  * Space -
  */
+private fun maxEqualRowsAfterFlips(matrix: Array<IntArray>): Int {
+    val counter = mutableMapOf<String, Int>()
+    val sb = StringBuilder()
+    var shouldFlip: Boolean
+    matrix.forEach { row ->
+        shouldFlip = row[0] == 0
+        sb.clear()
+        row.forEach { num ->
+            if (shouldFlip) {
+                if (num == 0) {
+                    sb.append(1)
+                } else {
+                    sb.append(0)
+                }
+            } else {
+                sb.append(num)
+            }
+        }
+        val string = sb.toString()
+        counter[string] = counter.getOrDefault(string, 0) + 1
+    }
+    return counter.values.max()
+}
+
 private fun main() {
 
 }
--- src/main/java/leetcode/leetcode_2257_count_unguarded_cells_in_the_grid/Solution.kt
+++ src/main/java/leetcode/leetcode_2257_count_unguarded_cells_in_the_grid/Solution.kt
@@ -1,19 +1,20 @@
 package leetcode.leetcode_2257_count_unguarded_cells_in_the_grid
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/count-unguarded-cells-in-the-grid/?envType=daily-question&envId=2024-11-21
  *
  * TODO(Abhi) - To revisit
  *
  * Using
  *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
  *
  * Time -
  * Space -
  */
+
 private fun main() {
 
 }
