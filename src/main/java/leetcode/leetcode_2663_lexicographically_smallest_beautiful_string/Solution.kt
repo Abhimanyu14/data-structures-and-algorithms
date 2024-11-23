cd0612f26570f8f7fe127bb220ac37214f235ee1
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-23 18:47:45 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2663_lexicographically_smallest_beautiful_string/Solution.kt
+++ src/main/java/leetcode/leetcode_2663_lexicographically_smallest_beautiful_string/Solution.kt
@@ -5,53 +5,41 @@ package leetcode.leetcode_2663_lexicographically_smallest_beautiful_string
  *
  * TODO(Abhi) - To revisit
  *
+ * Source - https://leetcode.com/problems/lexicographically-smallest-beautiful-string/discuss/3468265/Weird-Problem
+ *
  * Using
  *
- * Difficulty -
+ * Difficulty - Hard
  *
  * Stats
+ * Runtime: 15 ms, faster than 80.00%
+ * Memory Usage: 40.3 MB, less than 70.00%
  *
  * Time -
  * Space -
  */
 private fun smallestBeautifulString(s: String, k: Int): String {
-    val characters: CharArray = s.toCharArray()
-    fun increment(): Boolean {
-        var index = characters.lastIndex
-        while (index >= 0) {
-            if (characters[index] - 'a' + 1 == k) {
-                characters[index] = 'a'
-                index++
-            } else {
-                characters[index]++
-                return true
-            }
-        }
-        return false
+    val chars = s.toCharArray()
+    fun isValid(i: Int): Boolean {
+        return (i < 1 || chars[i] != chars[i - 1]) && (i < 2 || chars[i] != chars[i - 2])
     }
-
-    fun isBeautiful(): Boolean {
-        for (i in 0..<characters.lastIndex) {
-            if (characters[i] == characters[i + 1]) {
-                return false
-            }
+    for (i in chars.lastIndex downTo 0) {
+        chars[i]++
+        while (!isValid(i)) {
+            chars[i]++
         }
-        for (i in 0..(characters.lastIndex - 2)) {
-            if (characters[i] == characters[i + 2]) {
-                return false
+        if (chars[i] < 'a' + k) {
+            for (j in i + 1..<chars.size) {
+                chars[j] = 'a'
+                while (!isValid(j)) {
+                    chars[j]++
+                }
             }
-        }
-        return true
-    }
-
-    while (increment()) {
-        if (isBeautiful()) {
-            return characters.joinToString("")
+            return String(chars)
         }
     }
     return ""
 }
-
 
 private fun main() {
 
