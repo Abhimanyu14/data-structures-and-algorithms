81d100562ad05076b3f3940e1515b35619087e7c
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-23 09:20:51 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1861_rotating_the_box/Solution.kt
+++ src/main/java/leetcode/leetcode_1861_rotating_the_box/Solution.kt
@@ -1,19 +1,36 @@
 package leetcode.leetcode_1861_rotating_the_box
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/rotating-the-box/
  *
- * TODO(Abhi) - To revisit
+ * Using iteration
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 784 ms, faster than 40.00%
+ * Memory Usage: 87.1 MB, less than 52.94%
  *
- * Time -
- * Space -
+ * Time - O(M * N)
+ * Space - O(M * N)
  */
+private fun rotateTheBox(box: Array<CharArray>): Array<CharArray> {
+    val result = Array(box[0].size) { CharArray(box.size) { '.' } }
+    for (i in box.indices) {
+        var current = box[0].lastIndex
+        for (j in box[0].lastIndex downTo 0) {
+            if (box[i][j] == '*') {
+                result[j][box.lastIndex - i] = '*'
+                current = j - 1
+            } else if (box[i][j] == '#') {
+                result[current][box.lastIndex - i] = '#'
+                current--
+            }
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
