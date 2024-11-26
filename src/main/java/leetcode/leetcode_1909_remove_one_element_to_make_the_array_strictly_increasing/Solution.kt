1e9f3377fd18765a71b23a92fe0520a1f6e0e083
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-26 21:36:17 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1909_remove_one_element_to_make_the_array_strictly_increasing/Solution.kt
+++ src/main/java/leetcode/leetcode_1909_remove_one_element_to_make_the_array_strictly_increasing/Solution.kt
@@ -1,19 +1,41 @@
 package leetcode.leetcode_1909_remove_one_element_to_make_the_array_strictly_increasing
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
  *
- * TODO(Abhi) - To revisit
+ * Using iteration
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Easy
  *
  * Stats
+ * Runtime: 17 ms, faster than 100.00%
+ * Memory Usage: 38.8 MB, less than 25.00%
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(1)
  */
+private fun canBeIncreasing(nums: IntArray): Boolean {
+    var prev = nums[0]
+    var removed = false
+    for (i in 1..nums.lastIndex) {
+        if (nums[i] > prev) {
+            prev = nums[i]
+        } else {
+            if (removed) {
+                return false
+            } else if (i == nums.lastIndex) {
+                return true
+            } else {
+                removed = true
+                if (i == 1 || nums[i] > nums[i - 2]) {
+                    prev = nums[i]
+                }
+            }
+        }
+    }
+    return true
+}
+
 private fun main() {
 
 }
--- src/main/java/leetcode/leetcode_2924_find_champion_ii/Solution.kt
+++ src/main/java/leetcode/leetcode_2924_find_champion_ii/Solution.kt
@@ -1,19 +1,34 @@
 package leetcode.leetcode_2924_find_champion_ii
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/find-champion-ii/?envType=daily-question&envId=2024-11-26
  *
- * TODO(Abhi) - To revisit
+ * Using set
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 19 ms, faster than 16.67%
+ * Memory Usage: 59.4 MB, less than 33.33%
  *
  * Time -
  * Space -
  */
+private fun findChampion(n: Int, edges: Array<IntArray>): Int {
+    val set = mutableSetOf<Int>()
+    repeat(n) {
+        set.add(it)
+    }
+    edges.forEach {
+        set.remove(it[1])
+    }
+    return if (set.size == 1) {
+        set.first()
+    } else {
+        -1
+    }
+}
+
 private fun main() {
 
 }
