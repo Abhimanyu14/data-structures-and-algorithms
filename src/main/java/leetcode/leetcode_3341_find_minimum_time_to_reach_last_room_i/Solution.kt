0af91c7e13df992dda74e2a60d3a6208437bdcaa
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-29 13:06:12 +0530

    leetcode

--- src/main/java/leetcode/leetcode_3341_find_minimum_time_to_reach_last_room_i/Solution.kt
+++ src/main/java/leetcode/leetcode_3341_find_minimum_time_to_reach_last_room_i/Solution.kt
@@ -1,19 +1,69 @@
 package leetcode.leetcode_3341_find_minimum_time_to_reach_last_room_i
 
+import java.util.PriorityQueue
+import kotlin.math.max
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/
  *
  * TODO - To revisit
  *
- * Using
+ * Data Structure - Priority Queue, Array and List
+ * Algorithm - Dijkstra
  *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 266 ms, faster than 28.57%
+ * Memory Usage: 41.7 MB, less than 66.07%
  *
  * Time -
  * Space -
  */
+private data class Point(
+    val x: Int,
+    val y: Int,
+)
+
+private fun minTimeToReach(moveTime: Array<IntArray>): Int {
+    val visited = Array(moveTime.size) {
+        BooleanArray(moveTime[0].size)
+    }
+    val priorityQueue = PriorityQueue<Pair<Point, Int>> { a, b ->
+        a.second - b.second
+    }
+    fun getNextMoves(point: Point): List<Point> {
+        val nextPoints = mutableListOf<Point>()
+        if (point.x > 0) {
+            nextPoints.add(Point(point.x - 1, point.y))
+        }
+        if (point.y > 0) {
+            nextPoints.add(Point(point.x, point.y - 1))
+        }
+        if (point.x < moveTime.lastIndex) {
+            nextPoints.add(Point(point.x + 1, point.y))
+        }
+        if (point.y < moveTime[0].lastIndex) {
+            nextPoints.add(Point(point.x, point.y + 1))
+        }
+        return nextPoints
+    }
+    priorityQueue.offer(Pair(Point(0, 0), 0))
+    while (priorityQueue.isNotEmpty()) {
+        val (currentPoint, currentTime) = priorityQueue.poll()
+        if (!visited[currentPoint.x][currentPoint.y]) {
+            visited[currentPoint.x][currentPoint.y] = true
+            getNextMoves(currentPoint).forEach { nextPoint ->
+                if (nextPoint.x  == moveTime.lastIndex && nextPoint.y == moveTime[0].lastIndex) {
+                    return max(currentTime + 1, moveTime[nextPoint.x][nextPoint.y] + 1)
+                }
+                priorityQueue.offer(Pair(nextPoint, max(currentTime + 1, moveTime[nextPoint.x][nextPoint.y] + 1)))
+            }
+        }
+    }
+    return -1
+}
+
 private fun main() {
 
 }
--- src/main/java/leetcode/leetcode_3342_find_minimum_time_to_reach_last_room_ii/Solution.kt
+++ src/main/java/leetcode/leetcode_3342_find_minimum_time_to_reach_last_room_ii/Solution.kt
@@ -1,19 +1,74 @@
 package leetcode.leetcode_3342_find_minimum_time_to_reach_last_room_ii
 
+import java.util.PriorityQueue
+import kotlin.math.max
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/find-minimum-time-to-reach-last-room-ii/
  *
  * TODO - To revisit
  *
- * Using
+ * Data Structure - Priority Queue, Array and List
+ * Algorithm - Dijkstra
  *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 251 ms, faster than 86.49%
+ * Memory Usage: 133.3 MB, less than 86.49%
  *
  * Time -
  * Space -
  */
+private data class Move(
+    val x: Int,
+    val y: Int,
+    val time: Int,
+    val lastMoveIsOne: Boolean,
+)
+
+private fun minTimeToReach(moveTime: Array<IntArray>): Int {
+    val visited = Array(moveTime.size) {
+        BooleanArray(moveTime[0].size)
+    }
+    val priorityQueue = PriorityQueue<Move> { a, b ->
+        a.time - b.time
+    }
+    priorityQueue.offer(Move(0, 0, 0, false))
+    fun getNextPoints(x: Int, y: Int): List<Pair<Int, Int>> {
+        val nextPoints = mutableListOf<Pair<Int, Int>>()
+        if (x > 0) {
+            nextPoints.add(Pair(x - 1, y))
+        }
+        if (y > 0) {
+            nextPoints.add(Pair(x, y - 1))
+        }
+        if (x < moveTime.lastIndex) {
+            nextPoints.add(Pair(x + 1, y))
+        }
+        if (y < moveTime[0].lastIndex) {
+            nextPoints.add(Pair(x, y + 1))
+        }
+        return nextPoints
+    }
+
+    while (priorityQueue.isNotEmpty()) {
+        val currentMove = priorityQueue.poll()
+        if (!visited[currentMove.x][currentMove.y]) {
+            visited[currentMove.x][currentMove.y] = true
+            getNextPoints(currentMove.x, currentMove.y).forEach { (nextX, nextY) ->
+                val nextMoveTime = max(currentMove.time, moveTime[nextX][nextY]) + if (currentMove.lastMoveIsOne) { 2 } else { 1 }
+                if (nextX == moveTime.lastIndex && nextY == moveTime[0].lastIndex) {
+                    return nextMoveTime
+                }
+                priorityQueue.offer(Move(nextX, nextY, nextMoveTime, !currentMove.lastMoveIsOne))
+            }
+        }
+    }
+    return -1
+}
+
+
 private fun main() {
 
 }
