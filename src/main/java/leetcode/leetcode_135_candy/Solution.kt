3af5ab7983b957f1cd23ad319cf0c6a76d36c7ba
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-30 00:09:15 +0530

    leetcode

--- src/main/java/leetcode/leetcode_135_candy/Solution.kt
+++ src/main/java/leetcode/leetcode_135_candy/Solution.kt
@@ -7,33 +7,41 @@ import kotlin.math.max
  *
  * TODO(Abhi) - To revisit
  *
- * Using slope
+ * Data Structure - NA
+ * Algorithm - slope
  *
  * Stats
  * Runtime: 239 ms, faster than 56.48%
  * Memory Usage: 39.5 MB, less than 74.07%
+ *
+ * Time -
+ * Space -
  */
-private fun count(n: Int): Int {
-    return n * (n + 1) / 2
-}
-
 private fun candy(ratings: IntArray): Int {
     if (ratings.size <= 1) {
         return ratings.size
+    }
+
+    fun count(n: Int): Int {
+        return n * (n + 1) / 2
+    }
+
+    fun getSlope(prev: Int, current: Int): Int {
+        return if (current > prev) {
+            1
+        } else if (current < prev) {
+            -1
+        } else {
+            0
+        }
     }
 
     var candies = 0
     var up = 0
     var down = 0
     var oldSlope = 0
-    for (i in 1 until ratings.size) {
-        val newSlope = if (ratings[i] > ratings[i - 1]) {
-            1
-        } else if (ratings[i] < ratings[i - 1]) {
-            -1
-        } else {
-            0
-        }
+    for (i in 1..ratings.lastIndex) {
+        val newSlope = getSlope(ratings[i - 1], ratings[i])
         if ((oldSlope > 0 && newSlope == 0) || (oldSlope < 0 && newSlope >= 0)) {
             candies += count(up) + count(down) + max(up, down)
             up = 0
@@ -50,6 +58,43 @@ private fun candy(ratings: IntArray): Int {
     }
     candies += count(up) + count(down) + max(up, down) + 1
     return candies
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/candy/
+ *
+ * Data Structure - Array
+ * Algorithm - Slope
+ *
+ * Stats
+ * Runtime: 24 ms, faster than 18.90%
+ * Memory Usage: 41.3 MB, less than 14.08%
+ *
+ * Time - O(N)
+ * Space - O(N)
+ */
+private fun candyUsingTwoArray(ratings: IntArray): Int {
+    var result = 0
+    val left = IntArray(ratings.size)
+    val right = IntArray(ratings.size)
+    left[0] = 1
+    right[ratings.lastIndex] = 1
+    for (i in 1..ratings.lastIndex) {
+        left[i] = if (ratings[i] > ratings[i - 1]) {
+            left[i - 1] + 1
+        } else {
+            1
+        }
+        right[ratings.lastIndex - i] = if (ratings[ratings.lastIndex - i] > ratings[ratings.lastIndex - i + 1]) {
+            right[ratings.lastIndex - i + 1] + 1
+        } else {
+            1
+        }
+    }
+    for (i in ratings.indices) {
+        result += max(left[i], right[i])
+    }
+    return result
 }
 
 private fun main() {
