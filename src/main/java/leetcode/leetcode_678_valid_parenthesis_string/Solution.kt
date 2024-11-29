d44014df521a856b2b464079dde979f3bb66a7c3
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-11-29 20:06:06 +0530

    leetcode

--- src/main/java/leetcode/leetcode_678_valid_parenthesis_string/Solution.kt
+++ src/main/java/leetcode/leetcode_678_valid_parenthesis_string/Solution.kt
@@ -1,9 +1,54 @@
 package leetcode.leetcode_678_valid_parenthesis_string
 
+import kotlin.math.max
+
 /**
  * leetcode - https://leetcode.com/problems/valid-parenthesis-string/
  *
- * Using two pointers
+ * Data Structure - NA
+ * Algorithm - Using ranges
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ * Runtime: 0 ms, faster than 100.00%
+ * Memory Usage: 35.2 MB, less than 25.00%
+ *
+ * Time - O(3 ^ N)
+ * Space - O(N) - Recursion stack
+ */
+private fun checkValidString(s: String): Boolean {
+    var minimum = 0
+    var maximum = 0
+    s.forEach {
+        when (it) {
+            '(' -> {
+                minimum++
+                maximum++
+            }
+
+            ')' -> {
+                minimum = max(minimum - 1, 0)
+                maximum--
+                if (maximum < 0){
+                    return false
+                }
+            }
+
+            else -> {
+                minimum = max(minimum - 1, 0)
+                maximum++
+            }
+        }
+    }
+    return minimum == 0
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/valid-parenthesis-string/
+ *
+ * Data Structure - NA
+ * Algorithm - Two pointers
  *
  * Difficulty - Medium
  *
@@ -14,7 +59,7 @@ package leetcode.leetcode_678_valid_parenthesis_string
  * Time - O(N)
  * Space - O(1)
  */
-private fun checkValidString(s: String): Boolean {
+private fun checkValidStringUsingTwoPointer(s: String): Boolean {
     var openCount = 0
     var closeCount = 0
 
@@ -42,6 +87,81 @@ private fun checkValidString(s: String): Boolean {
 
     // If open count and close count are both non-negative, the string is valid
     return true
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/valid-parenthesis-string/
+ *
+ * Data Structure - Array
+ * Algorithm - Dynamic programming - Memoization
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ * Runtime: 4 ms, faster than 39.39%
+ * Memory Usage: 36.4 MB, less than 14.58%
+ *
+ * Time - O(3 ^ N)
+ * Space - O(N) - Recursion stack
+ */
+private fun checkValidStringUsingMemoization(s: String): Boolean {
+    val dp = Array(s.length) {
+        Array<Boolean?>(s.length) { null }
+    }
+
+    fun isValid(index: Int, count: Int): Boolean {
+        if (count < 0) {
+            return false
+        }
+        if (index == s.length) {
+            return count == 0
+        }
+        dp[index][count]?.let {
+            return it
+        }
+        dp[index][count] = if (s[index] == '(') {
+            isValid(index + 1, count + 1)
+        } else if (s[index] == ')') {
+            isValid(index + 1, count - 1)
+        } else {
+            (isValid(index + 1, count + 1) || isValid(index + 1, count) || isValid(index + 1, count - 1))
+        }
+        return dp[index][count] ?: false
+    }
+    return isValid(0, 0)
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/valid-parenthesis-string/
+ *
+ * Data Structure - NA
+ * Algorithm - Recursion
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ * Time Limit Exceeded
+ *
+ * Time - O(3 ^ N)
+ * Space - O(N) - Recursion stack
+ */
+private fun checkValidStringUsingRecursion(s: String): Boolean {
+    fun isValid(index: Int, count: Int): Boolean {
+        if (count < 0) {
+            return false
+        }
+        if (index == s.length) {
+            return count == 0
+        }
+        return if (s[index] == '(') {
+            isValid(index + 1, count + 1)
+        } else if (s[index] == ')') {
+            isValid(index + 1, count - 1)
+        } else {
+            (isValid(index + 1, count + 1) || isValid(index + 1, count) || isValid(index + 1, count - 1))
+        }
+    }
+    return isValid(0, 0)
 }
 
 private fun main() {
