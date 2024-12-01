f9c7218d11a23f6e64ff80801f9f7785b56edd6e
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-01 13:47:08 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1346_check_if_n_and_its_double_exist/Solution.kt
+++ src/main/java/leetcode/leetcode_1346_check_if_n_and_its_double_exist/Solution.kt
@@ -3,11 +3,15 @@ package leetcode.leetcode_1346_check_if_n_and_its_double_exist
 /**
  * leetcode - https://leetcode.com/problems/check-if-n-and-its-double-exist/
  *
- * Using set
+ * Data Structure - Set
+ * Algorithm - Iteration
  *
  * Stats
- * Runtime: 188 ms, faster than 28.12%
- * Memory Usage: 36.2 MB, less than 96.88%
+ * Runtime: 3 ms, faster than 85.29%
+ * Memory Usage: 38.1 MB, less than 22.81%
+ *
+ * Time - O(N)
+ * Space - O(N)
  */
 fun checkIfExist(arr: IntArray): Boolean {
     val set = mutableSetOf<Int>()
