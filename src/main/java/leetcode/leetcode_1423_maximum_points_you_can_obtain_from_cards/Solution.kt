bf60b1a67724b6942c73759b74f70ae93cf2e60e
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-01 12:26:05 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1423_maximum_points_you_can_obtain_from_cards/Solution.kt
+++ src/main/java/leetcode/leetcode_1423_maximum_points_you_can_obtain_from_cards/Solution.kt
@@ -5,31 +5,32 @@ import kotlin.math.max
 /**
  * leetcode - https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
  *
- * Using sliding window
+ * Data Structure - NA
+ * Algorithm - Sliding Window
  *
  * Difficulty - Medium
  *
  * Stats
- * Runtime: 20 ms, faster than 50.00%
- * Memory Usage: 51.1 MB, less than 82.35%
+ * Runtime: 21 ms, faster than 34.78%
+ * Memory Usage: 51.6 MB, less than 37.50%
  *
  * Time - O(N)
  * Space - O(1)
  */
 private fun maxScore(cardPoints: IntArray, k: Int): Int {
-    var result = 0
-    var current = 0
-    var index = 0
-    while (index < k) {
-        result += cardPoints[index]
-        index++
+    var result: Int
+    var totalSum = 0
+    var currentWindowSum = 0
+    for (i in cardPoints.indices) {
+        totalSum += cardPoints[i]
+        if (i == cardPoints.lastIndex - k) {
+            currentWindowSum = cardPoints[i]
+        }
     }
-    current = result
-    while (index > 0) {
-        current -= cardPoints[index - 1]
-        current += cardPoints[cardPoints.lastIndex - (k - index)]
-        result = max(result, current)
-        index--
+    result = totalSum - currentWindowSum
+    repeat(k) {
+        currentWindowSum = currentWindowSum - cardPoints[it] + cardPoints[cardPoints.size - k + it]
+        result = max(result, totalSum - currentWindowSum)
     }
     return result
 }
@@ -37,3 +38,10 @@ private fun maxScore(cardPoints: IntArray, k: Int): Int {
 private fun main() {
 
 }
+
+
+/*
+
+[-2,-1,-3,-6,4]
+[1,1,1,1,1,5,5]
+[6,-31,50,-35,41,37,-42,13]*/
