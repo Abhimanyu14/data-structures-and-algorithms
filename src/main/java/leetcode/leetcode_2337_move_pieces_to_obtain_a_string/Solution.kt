b3b048fe59ee925e219854084161e7f3a4d099e3
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-05 09:18:18 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2337_move_pieces_to_obtain_a_string/Solution.kt
+++ src/main/java/leetcode/leetcode_2337_move_pieces_to_obtain_a_string/Solution.kt
@@ -3,59 +3,51 @@ package leetcode.leetcode_2337_move_pieces_to_obtain_a_string
 /**
  * leetcode - https://leetcode.com/problems/move-pieces-to-obtain-a-string/
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Two Pointers
+ * Algorithm - Iteration
  *
- * Using
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 18 ms, faster than 100.00%
+ * Memory Usage: 39.6 MB, less than 100.00%
  *
+ * Time -
+ * Space -
  */
 private fun canChange(start: String, target: String): Boolean {
-    val n = target.length
-    var left = 0
-    var right = 0
-    var temp: Int
-    while (right < n) {
-        if (start[left] == target[right]) {
-            left++
-            right++
-        } else {
-            if (target[right] == 'L') {
-                temp = left
-                while (temp < n && start[temp] == '_') {
-                    temp++
-                }
-                if (temp == n || start[temp] == 'R') {
-                    return false
-                }
-                if (start[temp] == 'L') {
-//                    [left] = start[temp].also {
-//                        start[temp] = start[left]
-//                    }
-                    left++
-                    right++
-                }
-            } else if (target[right] == 'R') {
+    var leftCount = 0
+    var rightCount = 0
+    for (i in start.indices) {
+        if (target[i] == 'L') {
+            if (rightCount > 0) {
                 return false
-            } else {
-                temp = left
-                while (temp < n && start[temp] == 'R') {
-                    temp++
-                }
-                if (temp == n || start[temp] == 'L') {
-                    return false
-                }
-                if (start[temp] == '_') {
-//                    start[left] = start[temp].also {
-//                        start[temp] = start[left]
-//                    }
-                    left++
-                    right++
-                }
             }
+            leftCount--
+        } else if (target[i] == 'R') {
+            if (leftCount < 0) {
+                return false
+            }
+            rightCount--
+        }
+
+        if (start[i] == 'L') {
+            if (rightCount > 0) {
+                return false
+            }
+            leftCount++
+        } else if (start[i] == 'R') {
+            if (leftCount < 0) {
+                return false
+            }
+            rightCount++
+        }
+
+        if ((leftCount > 0) || (rightCount < 0)) {
+            return false
         }
     }
-    return true
+    return !(leftCount != 0 || rightCount != 0)
 }
 
 private fun main() {
