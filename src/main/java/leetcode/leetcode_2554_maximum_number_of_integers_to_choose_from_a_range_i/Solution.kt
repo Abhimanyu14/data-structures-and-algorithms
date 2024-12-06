cf70448ea7569e40d2ad36ed6bada3e1b7be6767
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-06 17:47:15 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2554_maximum_number_of_integers_to_choose_from_a_range_i/Solution.kt
+++ src/main/java/leetcode/leetcode_2554_maximum_number_of_integers_to_choose_from_a_range_i/Solution.kt
@@ -1,19 +1,36 @@
 package leetcode.leetcode_2554_maximum_number_of_integers_to_choose_from_a_range_i
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/maximum-number-of-integers-to-choose-from-a-range-i/?envType=daily-question&envId=2024-12-06
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Set
+ * Algorithm - Sorting
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 67 ms, faster than 80.00%
+ * Memory Usage: 48.4 MB, less than 30.00%
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(N)
  */
+private fun maxCount(banned: IntArray, n: Int, maxSum: Int): Int {
+    var result = 0
+    var currentSum = 0
+    val bannedSet = banned.toSet()
+    for (i in 1..n) {
+        if (currentSum + i > maxSum) {
+            break
+        }
+        if (i !in bannedSet) {
+            result++
+            currentSum += i
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
