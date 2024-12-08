277f4932aa2ca3a25860fd9cbaf231c013be4043
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-08 21:36:48 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2550_count_collisions_of_monkeys_on_a_polygon/Solution.kt
+++ src/main/java/leetcode/leetcode_2550_count_collisions_of_monkeys_on_a_polygon/Solution.kt
@@ -1,19 +1,46 @@
 package leetcode.leetcode_2550_count_collisions_of_monkeys_on_a_polygon
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/count-collisions-of-monkeys-on-a-polygon/
  *
  * TODO(Abhi) - To revisit
  *
- * Using
+ * Data Structure - NA
+ * Algorithm - Modular Exponentiation
  *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 0 ms, faster than 100.00%
+ * Memory Usage: 34.5 MB, less than 33.33%
  *
  * Time -
  * Space -
  */
+private fun monkeyMove(n: Int): Int {
+    val mod = 1_000_000_007
+    fun modPow(base: Long, exp: Int, mod: Int): Int {
+        var result = 1L
+        var b = base
+        var e = exp
+        while (e > 0) {
+            if (e % 2 == 1) { // If `e` is odd
+                result = (result * b) % mod
+            }
+            b = (b * b) % mod
+            e /= 2
+        }
+        return result.toInt()
+    }
+
+    val power = modPow(2, n, mod) - 2
+    return if (power < 0) {
+        (power + mod)
+    } else {
+        power
+    }
+}
+
 private fun main() {
 
 }
