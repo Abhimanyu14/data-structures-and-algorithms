2f26734dad51ca67a4f6a77f24b528bb4334cb31
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-09 10:44:46 +0530

    leetcode

--- src/main/java/leetcode/leetcode_3152_special_array_ii/Solution.kt
+++ src/main/java/leetcode/leetcode_3152_special_array_ii/Solution.kt
@@ -1,19 +1,68 @@
 package leetcode.leetcode_3152_special_array_ii
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/special-array-ii/
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Array
+ * Algorithm - Iteration and Preprocessing
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 20 ms, faster than 50.00%
+ * Memory Usage: 124.7 MB, less than 33.33%
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(N)
  */
+private fun isArraySpecial(nums: IntArray, queries: Array<IntArray>): BooleanArray {
+    val result = BooleanArray(queries.size)
+    val previousSameParityIndex = IntArray(nums.size) { -1 }
+    for (i in 1..nums.lastIndex) {
+        previousSameParityIndex[i] = if (nums[i] % 2 == nums[i - 1] % 2) {
+            i - 1
+        } else {
+            previousSameParityIndex[i - 1]
+        }
+    }
+    for (i in queries.indices) {
+        result[i] = queries[i][0] > previousSameParityIndex[queries[i][1]]
+    }
+    return result
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/special-array-ii/
+ *
+ * Data Structure - NA
+ * Algorithm - Iteration
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ * Time Limit Exceeded
+ *
+ * nums.size - N
+ * queries.size - M
+ * Time - O(M * N)
+ * Space - O(1)
+ */
+private fun isArraySpecialUsingBruteForce(nums: IntArray, queries: Array<IntArray>): BooleanArray {
+    val result = BooleanArray(queries.size) { true }
+    var isEven: Boolean
+    for (i in queries.indices) {
+        isEven = nums[queries[i][0]] % 2 == 0
+        for (j in (queries[i][0] + 1)..queries[i][1]) {
+            if ((nums[j] % 2 == 0 && isEven) || (nums[j] % 2 != 0 && !isEven)) {
+                result[i] = false
+                break
+            }
+            isEven = !isEven
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
