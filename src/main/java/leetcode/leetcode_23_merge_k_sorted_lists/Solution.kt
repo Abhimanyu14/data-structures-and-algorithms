4c27d54c8d0f77b1c8a30b9bcb7ce2e3c7369d98
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-13 17:31:10 +0530

    leetcode

--- src/main/java/leetcode/leetcode_23_merge_k_sorted_lists/Solution.kt
+++ src/main/java/leetcode/leetcode_23_merge_k_sorted_lists/Solution.kt
@@ -1,19 +1,49 @@
 package leetcode.leetcode_23_merge_k_sorted_lists
 
+import data_structures_and_algorithms.linkedlist.ListNode
+import java.util.PriorityQueue
+
 /**
  * leetcode - https://leetcode.com/problems/merge-k-sorted-lists/description/?envType=company&envId=google&favoriteSlug=google-thirty-days
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - PriorityQueue
+ * Algorithm - Heapify
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Hard
  *
  * Stats
+ * Runtime: 47 ms, faster than 26.94%
+ * Memory Usage: 39.3 MB, less than 63.66%
  *
  * Time -
  * Space -
  */
+private fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+    val dummy = ListNode(-1)
+    var currentNode: ListNode = dummy
+    val priorityQueue = PriorityQueue<Pair<Int, ListNode>> { a, b -> // value, list, index
+        if (a.second.`val` == b.second.`val`) {
+            a.first - b.first
+        } else {
+            a.second.`val` - b.second.`val`
+        }
+    }
+    for (i in lists.indices) {
+        lists[i]?.let {
+            priorityQueue.offer(Pair(i, it))
+        }
+    }
+    while (priorityQueue.isNotEmpty()) {
+        val current = priorityQueue.poll()
+        currentNode.next = current.second
+        currentNode = currentNode.next!!
+        current.second.next?.let {
+            priorityQueue.offer(Pair(current.first, it))
+        }
+    }
+    return dummy.next
+}
+
 private fun main() {
 
 }
--- src/main/java/leetcode/leetcode_355_design_twitter/Solution.kt
+++ src/main/java/leetcode/leetcode_355_design_twitter/Solution.kt
@@ -1,19 +1,68 @@
 package leetcode.leetcode_355_design_twitter
 
+import java.util.PriorityQueue
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/design-twitter?envType=company&envId=google&favoriteSlug=google-thirty-days&difficulty=MEDIUM
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Map and List
+ * Algorithm - Heapify, Hashing and Iteration
  *
- * Using
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 100 ms, faster than 15.00%
+ * Memory Usage: 60.3 MB, less than 17.31%
  *
  * Time -
  * Space -
  */
+private class Twitter() {
+    private var time = 0
+    private val tweets = mutableMapOf<Int, MutableList<Pair<Int, Int>>>()
+    private val followers = mutableMapOf<Int, MutableSet<Int>>()
+
+    fun postTweet(userId: Int, tweetId: Int) {
+        time++
+        tweets.computeIfAbsent(userId) { mutableListOf() }.add(Pair(time, tweetId))
+    }
+
+    fun getNewsFeed(userId: Int): List<Int> {
+        val result = mutableListOf<Int>()
+        val feed = PriorityQueue<IntArray> {  a, b -> // time, tweetId, userId, index
+            b[0] - a[0]
+        }
+        tweets[userId]?.last()?.let { (time, tweetId) ->
+            feed.offer(intArrayOf(time, tweetId, userId, tweets[userId]?.lastIndex ?: 0))
+        }
+        followers[userId]?.forEach { followeeId ->
+            tweets[followeeId]?.last()?.let { (time, tweetId) ->
+                feed.offer(intArrayOf(time, tweetId, followeeId, tweets[followeeId]?.lastIndex ?: 0))
+            }
+        }
+        var i = 0
+        while (i < 10 && feed.isNotEmpty()) {
+            val current = feed.poll()
+            result.add(current[1])
+            if (current[3] > 0) {
+                tweets[current[2]]?.getOrNull(current[3] - 1)?.let { (time, tweetId) ->
+                    feed.offer(intArrayOf(time, tweetId, current[2], current[3] - 1))
+                }
+            }
+            i++
+        }
+        return result
+    }
+
+    fun follow(followerId: Int, followeeId: Int) {
+        followers.computeIfAbsent(followerId) { mutableSetOf() }.add(followeeId)
+    }
+
+    fun unfollow(followerId: Int, followeeId: Int) {
+        followers[followerId]?.remove(followeeId)
+    }
+}
+
 private fun main() {
 
 }
