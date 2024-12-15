c7e52a7cf1987d524d6d132a5c9433a346babb40
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-15 12:09:09 +0530

    leetcode

--- src/main/java/leetcode/leetcode_15_3sum/Solution.kt
+++ src/main/java/leetcode/leetcode_15_3sum/Solution.kt
@@ -3,6 +3,48 @@ package leetcode.leetcode_15_3sum
 /**
  * leetcode - https://leetcode.com/problems/3sum/description/?envType=company&envId=google&favoriteSlug=google-thirty-days
  *
+ * Data Structure - Set and List
+ * Algorithm - Sorting
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ * Runtime: 235 ms, faster than 42.76%
+ * Memory Usage: 58.2 MB, less than 32.71%
+ *
+ * Time - O(N ^ 2)
+ * Space - O(N)
+ */
+private fun threeSum(nums: IntArray): List<List<Int>> {
+    val result = mutableSetOf<List<Int>>()
+    val sortedNums = nums.sorted()
+    for (i in 0..(sortedNums.lastIndex - 2)) {
+        var j = i + 1
+        var k = sortedNums.lastIndex
+        while (j < k) {
+            if (sortedNums[i] + sortedNums[j] + sortedNums[k] == 0) {
+                result.add(listOf(sortedNums[i], sortedNums[j], sortedNums[k]).sorted())
+                j++
+                k--
+                while (j < k && sortedNums[j] == sortedNums[j - 1]) {
+                    j++
+                }
+                while (j < k && sortedNums[k] == sortedNums[k + 1]) {
+                    k--
+                }
+            } else if (sortedNums[i] + sortedNums[j] + sortedNums[k] > 0) {
+                k--
+            } else {
+                j++
+            }
+        }
+    }
+    return result.toList()
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/3sum/description/?envType=company&envId=google&favoriteSlug=google-thirty-days
+ *
  * Data Structure - 2 Sets
  * Algorithm - Iteration and Hashing
  *
@@ -15,7 +57,7 @@ package leetcode.leetcode_15_3sum
  * Time - O(N ^ 2)
  * Space -
  */
-private fun threeSum(nums: IntArray): List<List<Int>> {
+private fun threeSumUsingSets(nums: IntArray): List<List<Int>> {
     val result = mutableSetOf<List<Int>>()
     val set = mutableSetOf<Int>()
     for (i in 0..(nums.lastIndex - 1)) {
