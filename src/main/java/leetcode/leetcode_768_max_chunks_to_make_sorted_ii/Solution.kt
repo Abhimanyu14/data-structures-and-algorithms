8a6e627a0667bed51b135843eb0d6598bdabf479
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-20 06:49:54 +0530

    leetcode

--- src/main/java/leetcode/leetcode_768_max_chunks_to_make_sorted_ii/Solution.kt
+++ src/main/java/leetcode/leetcode_768_max_chunks_to_make_sorted_ii/Solution.kt
@@ -1,20 +1,43 @@
 package leetcode.leetcode_768_max_chunks_to_make_sorted_ii
 
+import java.util.Stack
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/max-chunks-to-make-sorted-ii/
  *
  * TODO(Abhi) - To revisit
  *
- * Data Structure -
- * Algorithm -
+ * Data Structure - Monotonic Stack
+ * Algorithm - Monotonic Stack and Iteration
  *
- * Difficulty -
+ * Difficulty - Hard
+ *
+ * Note: The part 1 question gives the base idea
+ * https://leetcode.com/problems/max-chunks-to-make-sorted/solution/
  *
  * Stats
+ * Runtime: 6 ms, faster than 100.00%
+ * Memory Usage: 38.7 MB, less than 100.00%
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(N)
  */
+private fun maxChunksToSorted(arr: IntArray): Int {
+    val monotonicStack = Stack<Int>()
+    for (i in arr.indices) {
+        if (monotonicStack.isEmpty() || arr[i] >= monotonicStack.peek()) {
+            monotonicStack.push(arr[i])
+        } else {
+            val top = monotonicStack.peek()
+            while (monotonicStack.isNotEmpty() && monotonicStack.peek() > arr[i]) {
+                monotonicStack.pop()
+            }
+            monotonicStack.push(top)
+        }
+    }
+    return monotonicStack.size
+}
+
 private fun main() {
 
 }
