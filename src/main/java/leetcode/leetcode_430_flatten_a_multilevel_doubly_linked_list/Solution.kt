083086cf7c9ab2829816eb6a8398a03579806775
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-22 22:31:36 +0530

    leetcode

--- src/main/java/leetcode/leetcode_430_flatten_a_multilevel_doubly_linked_list/Solution.kt
+++ src/main/java/leetcode/leetcode_430_flatten_a_multilevel_doubly_linked_list/Solution.kt
@@ -1,20 +1,58 @@
 package leetcode.leetcode_430_flatten_a_multilevel_doubly_linked_list
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/?envType=problem-list-v2&envId=doubly-linked-list
  *
- * TODO(Abhi) - To revisit
- *
- * Data Structure -
+ * Data Structure - Doubly Linked List
  * Algorithm -
  *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 173 ms, faster than 27.78%
+ * Memory Usage: 37 MB, less than 5.56%
  *
  * Time -
  * Space -
  */
+private class Node(var `val`: Int) {
+    var prev: Node? = null
+    var next: Node? = null
+    var child: Node? = null
+}
+
+private fun flatten(root: Node?): Node? {
+    if (root == null) {
+        return null
+    }
+    fun flattenLevel(head: Node): Pair<Node, Node> {
+        var tail = head
+        while (tail.next != null) {
+            if (tail.child != null) {
+                val nextNode = tail.next!!
+                val (childHead, childTail) = flattenLevel(tail.child!!)
+                tail.next = childHead
+                childHead.prev = tail
+                nextNode.prev = childTail
+                childTail.next = nextNode
+                tail.child = null
+                tail = nextNode
+            } else {
+                tail = tail.next!!
+            }
+        }
+        if (tail.child != null) {
+            val (childHead, childTail) = flattenLevel(tail.child!!)
+            tail.next = childHead
+            childHead.prev = tail
+            tail.child = null
+            tail = childTail
+        }
+        return Pair(head, tail)
+    }
+    return flattenLevel(root).first
+}
+
 private fun main() {
 
 }
--- src/main/java/leetcode/leetcode_916_word_subsets/Solution.kt
+++ src/main/java/leetcode/leetcode_916_word_subsets/Solution.kt
@@ -1,20 +1,55 @@
 package leetcode.leetcode_916_word_subsets
 
+import kotlin.math.max
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/word-subsets/
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Array
+ * Algorithm - Counting
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 20 ms, faster than 83.33%
+ * Memory Usage: 50.7 MB, less than 83.33%
  *
  * Time -
  * Space -
  */
+private fun wordSubsets(words1: Array<String>, words2: Array<String>): List<String> {
+    val result = mutableListOf<String>()
+    fun getCounter(str: String): IntArray {
+        val counter = IntArray(26)
+        for (char in str) {
+            counter[char - 'a']++
+        }
+        return counter
+    }
+
+    val wordsCounter = IntArray(26)
+    for (word in words2) {
+        val counter = getCounter(word)
+        for (i in 0..25) {
+            wordsCounter[i] = max(wordsCounter[i], counter[i])
+        }
+    }
+    for (word in words1) {
+        val counter = getCounter(word)
+        var flag = true
+        for (i in 0..25) {
+            if (wordsCounter[i] > counter[i]) {
+                flag = false
+                break
+            }
+        }
+        if (flag) {
+            result.add(word)
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
