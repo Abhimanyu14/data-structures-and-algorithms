fd51a460e88169204c3e9f559377e9a224146f7e
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-23 10:44:09 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2360_longest_cycle_in_a_graph/Solution.kt
+++ src/main/java/leetcode/leetcode_2360_longest_cycle_in_a_graph/Solution.kt
@@ -1,20 +1,70 @@
 package leetcode.leetcode_2360_longest_cycle_in_a_graph
 
+import kotlin.math.max
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/longest-cycle-in-a-graph/
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Graph, Array, and Map
+ * Algorithm - Counting and DFS
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Hard
  *
  * Stats
+ * Runtime: 73 ms, faster than 100.00%
+ * Memory Usage: 58.4 MB, less than 100.00%
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(N)
  */
+private fun longestCycle(edges: IntArray): Int {
+    var result = -1
+    val visited = BooleanArray(edges.size)
+    for (i in edges.indices) {
+        val map = mutableMapOf<Int, Int>()
+        var time = 0
+        var currentNode = i
+        while (!visited[currentNode] && edges[currentNode] != -1 && !map.contains(edges[currentNode])) {
+            map[currentNode] = time
+            visited[currentNode] = true
+            currentNode = edges[currentNode]
+            time++
+        }
+        result = max(result, time + 1 - map.getOrDefault(edges[currentNode], time + 2))
+    }
+    return result
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/longest-cycle-in-a-graph/
+ *
+ * Data Structure - Graph, Array and Map
+ * Algorithm - DFS
+ *
+ * Difficulty - Hard
+ *
+ * Stats
+ * Time Limit Exceeded
+ *
+ * Time - O(N ^ 2)
+ * Space - O(N)
+ */
+private fun longestCycleUsingBruteForce(edges: IntArray): Int {
+    var result = -1
+    for (i in edges.indices) {
+        val map = mutableMapOf<Int, Int>()
+        var time = 0
+        var currentNode = i
+        while (edges[currentNode] != -1 && !map.contains(edges[currentNode])) {
+            map[currentNode] = time
+            currentNode = edges[currentNode]
+            time++
+        }
+        result = max(result, time + 1 - map.getOrDefault(edges[currentNode], time + 2))
+    }
+    return result
+}
+
 private fun main() {
 
 }
--- src/main/java/leetcode/leetcode_2471_minimum_number_of_operations_to_sort_a_binary_tree_by_level/Solution.kt
+++ src/main/java/leetcode/leetcode_2471_minimum_number_of_operations_to_sort_a_binary_tree_by_level/Solution.kt
@@ -1,20 +1,131 @@
 package leetcode.leetcode_2471_minimum_number_of_operations_to_sort_a_binary_tree_by_level
 
+import data_structures_and_algorithms.TreeNode
+import java.util.LinkedList
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/minimum-number-of-operations-to-sort-a-binary-tree-by-level/
  *
  * TODO(Abhi) - To revisit
  *
- * Data Structure -
- * Algorithm -
+ * Data Structure - Tree
+ * Algorithm - BFS and Map
  *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 83 ms, faster than 100.00%
+ * Memory Usage: 61.6 MB, less than 100.00%
  *
  * Time -
  * Space -
  */
+private fun minimumOperationsUsingMap(root: TreeNode?): Int {
+    if (root == null) {
+        return 0
+    }
+    fun getSwapCount(nums: IntArray): Int {
+        var swapCount = 0
+        val sortedNums = nums.sortedArray()
+        val positionMap = mutableMapOf<Int, Int>()
+        for (i in nums.indices) {
+            positionMap[nums[i]] = i
+        }
+        for (i in nums.indices) {
+            if (nums[i] != sortedNums[i]) {
+                swapCount++
+                // Update position of swapped values
+                val swapPosition = positionMap.getOrDefault(sortedNums[i], -1)
+                positionMap[nums[i]] = swapPosition
+                nums[swapPosition] = nums[i]
+            }
+        }
+        return swapCount
+    }
+
+    var result = 0
+    val queue = LinkedList<TreeNode>()
+    queue.add(root)
+    while (true) {
+        val queueSize = queue.size
+        if (queueSize == 0) {
+            break
+        }
+        val currentLevelValues = IntArray(queueSize)
+        for (i in 0..<queueSize) {
+            val currentNode = queue.poll()
+            currentLevelValues[i] = currentNode.`val`
+            currentNode.left?.let {
+                queue.add(it)
+            }
+            currentNode.right?.let {
+                queue.add(it)
+            }
+        }
+        result += getSwapCount(currentLevelValues)
+    }
+    return result
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/minimum-number-of-operations-to-sort-a-binary-tree-by-level/
+ *
+ * Data Structure - Tree
+ * Algorithm - BFS and Naive Sorting
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ * Runtime: 1562 ms, faster than 100.00%
+ * Memory Usage: 58.6 MB, less than 100.00%
+ *
+ * Time - O(N ^ 2)
+ * Space - O(N)
+ */
+fun minimumOperationsUsingNaiveSorting(root: TreeNode?): Int {
+    if (root == null) {
+        return 0
+    }
+    var result = 0
+    val currentQueue = mutableListOf<TreeNode>()
+    val nextQueue = mutableListOf<TreeNode>()
+    nextQueue.add(root)
+    while (true) {
+        if (nextQueue.size == 0) {
+            break
+        }
+        currentQueue.clear()
+        currentQueue.addAll(nextQueue)
+        nextQueue.clear()
+        for (i in currentQueue.indices) {
+            currentQueue[i].left?.let {
+                nextQueue.add(it)
+            }
+            currentQueue[i].right?.let {
+                nextQueue.add(it)
+            }
+        }
+
+        val processingArray = currentQueue.toTypedArray()
+        var candidateIndex: Int
+        for (i in 0..<processingArray.lastIndex) {
+            candidateIndex = i
+            for (j in (i + 1)..processingArray.lastIndex) {
+                if (processingArray[j].`val` < processingArray[candidateIndex].`val`) {
+                    candidateIndex = j
+                }
+            }
+            if (candidateIndex != i) {
+                result++
+                processingArray[i] = processingArray[candidateIndex].also {
+                    processingArray[candidateIndex] = processingArray[i]
+                }
+            }
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
