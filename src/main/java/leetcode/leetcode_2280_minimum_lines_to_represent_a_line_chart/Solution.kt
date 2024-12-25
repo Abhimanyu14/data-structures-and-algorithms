28ae43cf8f9cf4b8adc10092e35052eeaabdd3a1
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-25 17:24:32 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2280_minimum_lines_to_represent_a_line_chart/Solution.kt
+++ src/main/java/leetcode/leetcode_2280_minimum_lines_to_represent_a_line_chart/Solution.kt
@@ -1,20 +1,40 @@
 package leetcode.leetcode_2280_minimum_lines_to_represent_a_line_chart
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/minimum-lines-to-represent-a-line-chart/
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Array
+ * Algorithm - Sorting
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 75 ms, faster than 87.50%
+ * Memory Usage: 80.8 MB, less than 50.00%
  *
  * Time -
  * Space -
  */
+private fun minimumLines(stockPrices: Array<IntArray>): Int {
+    if (stockPrices.size == 1) {
+        return 0
+    }
+    val sortedStockPrices = stockPrices.sortedBy { it[0] }
+    var currentDeltaX = sortedStockPrices[1][0] - sortedStockPrices[0][0]
+    var currentDeltaY = sortedStockPrices[1][1] - sortedStockPrices[0][1]
+    var result = 1
+    for (i in 2..sortedStockPrices.lastIndex) {
+        val newDeltaX = sortedStockPrices[i][0] - sortedStockPrices[i - 1][0]
+        val newDeltaY = sortedStockPrices[i][1] - sortedStockPrices[i - 1][1]
+        if (currentDeltaX * newDeltaY != newDeltaX * currentDeltaY) {
+            result++
+            currentDeltaX = newDeltaX
+            currentDeltaY = newDeltaY
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
