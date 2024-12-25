2e06b8285df5ef8b49a4c988443c8752c903cd47
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-12-25 10:17:12 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1161_maximum_level_sum_of_a_binary_tree/Solution.kt
+++ src/main/java/leetcode/leetcode_1161_maximum_level_sum_of_a_binary_tree/Solution.kt
@@ -1,20 +1,54 @@
 package leetcode.leetcode_1161_maximum_level_sum_of_a_binary_tree
 
+import data_structures_and_algorithms.TreeNode
+import java.util.LinkedList
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Tree
+ * Algorithm - BFS
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 10 ms, faster than 89.34%
+ * Memory Usage: 43.8 MB, less than 66.39%
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(N)
  */
+private fun maxLevelSum(root: TreeNode?): Int {
+    if (root == null) {
+        return 0
+    }
+    var result = 0
+    val queue = LinkedList<TreeNode>()
+    var level = 0
+    var maxLevelSum = Int.MIN_VALUE
+    queue.offer(root)
+    while (queue.isNotEmpty()) {
+        val levelSize = queue.size
+        level++
+        var currentLevelSum = 0
+        for (i in 0..<levelSize) {
+            val current = queue.poll()
+            currentLevelSum += current.`val`
+            current.left?.let {
+                queue.offer(it)
+            }
+            current.right?.let {
+                queue.offer(it)
+            }
+        }
+        if (currentLevelSum > maxLevelSum) {
+            maxLevelSum = currentLevelSum
+            result = level
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
--- src/main/java/leetcode/leetcode_515_find_largest_value_in_each_tree_row/Solution.kt
+++ src/main/java/leetcode/leetcode_515_find_largest_value_in_each_tree_row/Solution.kt
@@ -1,18 +1,55 @@
 package leetcode.leetcode_515_find_largest_value_in_each_tree_row
 
 import data_structures_and_algorithms.TreeNode
+import java.util.LinkedList
 import kotlin.math.max
+
 
 /**
  * leetcode - https://leetcode.com/problems/find-largest-value-in-each-tree-row/
  *
- * Using recursion
+ * Data Structure - Tree
+ * Algorithm - BFS
+ *
+ * Difficulty - Medium
  *
  * Stats
+ * Runtime: 9 ms, faster than 37.21%
+ * Memory Usage: 39.7 MB, less than 18.60%
+ *
+ * Time - O(N)
+ * Space - O(N)
+ */
+private fun largestValues(root: TreeNode?): List<Int> {
+    if (root == null) {
+        return emptyList()
+    }
+    val result = mutableListOf<Int>()
+    val queue = LinkedList<TreeNode>()
+    queue.offer(root)
+    while (queue.isNotEmpty()) {
+        val size = queue.size
+        var currentMax = Int.MIN_VALUE
+        for (i in 0..<size) {
+            val current = queue.poll()
+            currentMax = max(currentMax, current.`val`)
+            current.left?.let {
+                queue.offer(it)
+            }
+            current.right?.let {
+                queue.offer(it)
+            }
+        }
+        result.add(currentMax)
+    }
+    return result
+}
+
+/**
  * Runtime: 214 ms, faster than 44.12%
  * Memory Usage: 37.9 MB, less than 55.88%
  */
-private fun largestValues(root: TreeNode?): List<Int> {
+private fun largestValuesUsingDfs(root: TreeNode?): List<Int> {
     val result = mutableListOf<Int>()
     fun dfs(head: TreeNode?, index: Int) {
         if (head == null) {
