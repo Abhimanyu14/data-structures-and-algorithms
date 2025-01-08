c056a01fae0747afad2422595e20326be3de6e47
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-01-08 19:24:43 +0530

    leetcode

--- src/main/java/leetcode/leetcode_3042_count_prefix_and_suffix_pairs_i/Solution.kt
+++ src/main/java/leetcode/leetcode_3042_count_prefix_and_suffix_pairs_i/Solution.kt
@@ -1,12 +1,12 @@
 package leetcode.leetcode_3042_count_prefix_and_suffix_pairs_i
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/?envType=daily-question&envId=2025-01-08
  *
  * TODO(Abhi) - To revisit
  *
  * Data Structure -
- * Algorithm -
+ * Algorithm - Brute Force
  *
  * Difficulty -
  *
@@ -15,6 +15,18 @@ package leetcode.leetcode_3042_count_prefix_and_suffix_pairs_i
  * Time -
  * Space -
  */
+private fun countPrefixSuffixPairs(words: Array<String>): Int {
+    var result = 0
+    for (i in 0..words.lastIndex) {
+        for (j in (i + 1)..words.lastIndex) {
+            if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
+                result++
+            }
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
