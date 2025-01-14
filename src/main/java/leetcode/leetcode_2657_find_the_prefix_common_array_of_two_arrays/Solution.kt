495a11165d2d4f63ff1ee076d4660ce974a56e23
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-01-14 21:14:19 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2657_find_the_prefix_common_array_of_two_arrays/Solution.kt
+++ src/main/java/leetcode/leetcode_2657_find_the_prefix_common_array_of_two_arrays/Solution.kt
@@ -1,20 +1,30 @@
 package leetcode.leetcode_2657_find_the_prefix_common_array_of_two_arrays
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/description/?envType=daily-question&envId=2025-01-14
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Set and Arrays
+ * Algorithm - Set intersection
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
  *
  * Time -
  * Space -
  */
+private fun findThePrefixCommonArray(A: IntArray, B: IntArray): IntArray {
+    val result = IntArray(A.size)
+    val setA = mutableSetOf<Int>()
+    val setB = mutableSetOf<Int>()
+    for (i in A.indices) {
+        setA.add(A[i])
+        setB.add(B[i])
+        result[i] = (setA intersect setB).size
+    }
+    return result
+}
+
 private fun main() {
 
 }
