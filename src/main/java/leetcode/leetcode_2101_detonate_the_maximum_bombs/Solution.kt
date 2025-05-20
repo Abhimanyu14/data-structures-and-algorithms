cd0abc466a7ec20fd528b5facb839caff46418ce
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-05-20 06:48:44 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2101_detonate_the_maximum_bombs/Solution.kt
+++ src/main/java/leetcode/leetcode_2101_detonate_the_maximum_bombs/Solution.kt
@@ -2,25 +2,72 @@ package leetcode.leetcode_2101_detonate_the_maximum_bombs
 
 import kotlin.math.max
 import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * leetcode - https://leetcode.com/problems/detonate-the-maximum-bombs/
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Graph
+ * Algorithm - BFS
  *
- * Using
+ * Difficulty - Medium
  *
  * Stats
  *
+ * Time - O(N^3)
+ * Space - O(N^2)
+ *
+ * Companies - Amazon, Google, Meta
  */
-private fun maximumDetonation(bombs: Array<IntArray>): Int {
+private fun maximumDetonationUsingBfs(bombs: Array<IntArray>): Int {
+    var result = 0
+    val graph = Array(bombs.size) {
+        mutableListOf<Int>()
+    }
+    val visited = mutableSetOf<Int>()
+    val queue = ArrayDeque<Int>()
+    for (i in bombs.indices) {
+        for (j in bombs.indices) {
+            if (i != j) {
+                val (x1, y1, d1) = bombs[i]
+                val (x2, y2, _) = bombs[j]
+                val distance = sqrt((x2 - x1).toDouble().pow(2) + (y2 - y1).toDouble().pow(2))
+                if (distance <= d1.toDouble()) {
+                    graph[i].add(j)
+                }
+            }
+        }
+    }
+    for (i in bombs.indices) {
+        visited.clear()
+        queue.addLast(i)
+        while (queue.isNotEmpty()) {
+            val current = queue.removeFirst()
+            if (visited.contains(current)) {
+                continue
+            }
+            visited.add(current)
+            graph[current].forEach {
+                if (!visited.contains(it)) {
+                    queue.addLast(it)
+                }
+            }
+        }
+        result = max(result, visited.size)
+    }
+    return result
+}
+
+private fun maximumDetonationUsingDfs(bombs: Array<IntArray>): Int {
     val graph = mutableMapOf<Int, List<Int>>()
     val adjacent = mutableListOf<Int>()
     bombs.forEachIndexed { index, bomb ->
         adjacent.clear()
         bombs.forEachIndexed { innerIndex, innerBomb ->
             if (index != innerIndex) {
-                if ((bomb[2] * bomb[2]).toDouble() >= ((innerBomb[0] - bomb[0]).toFloat().pow(2) + (innerBomb[1] - bomb[1]).toFloat().pow(2)).toDouble()) {
+                if ((bomb[2] * bomb[2]).toDouble() >= ((innerBomb[0] - bomb[0]).toFloat()
+                        .pow(2) + (innerBomb[1] - bomb[1]).toFloat().pow(2)).toDouble()
+                ) {
                     adjacent.add(innerIndex)
                 }
             }
@@ -43,6 +90,7 @@ private fun maximumDetonation(bombs: Array<IntArray>): Int {
         }
         return visited.size
     }
+
     var result = 0
     graph.forEach {
         result = max(result, dfs(it.key))
