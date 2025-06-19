780821df4e3614ac098a545bcbec13ad711e18ea
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-06-19 23:53:03 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2294_partition_array_such_that_maximum_difference_is_k/Solution.kt
+++ src/main/java/leetcode/leetcode_2294_partition_array_such_that_maximum_difference_is_k/Solution.kt
@@ -1,22 +1,33 @@
 package leetcode.leetcode_2294_partition_array_such_that_maximum_difference_is_k
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k/description/?envType=daily-question&envId=2025-06-19
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - [IntArray]
+ * Algorithm - Sorting
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
  *
- * Time -
- * Space -
+ * Time - O(N * log(N))
+ * Space - O(N)
  *
- * Companies -
+ * Companies - Amazon
  */
+private fun partitionArray(nums: IntArray, k: Int): Int {
+    var result = 1
+    val sortedNums = nums.sorted()
+    var minValue = sortedNums[0]
+    for (i in 1..sortedNums.lastIndex) {
+        if (sortedNums[i] > minValue + k) {
+            minValue = sortedNums[i]
+            result++
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
