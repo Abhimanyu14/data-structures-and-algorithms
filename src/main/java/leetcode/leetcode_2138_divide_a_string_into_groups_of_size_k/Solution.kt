d1fb8484a538d01a73db5f81c7b4d053a1cd4f35
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-06-22 13:56:30 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2138_divide_a_string_into_groups_of_size_k/Solution.kt
+++ src/main/java/leetcode/leetcode_2138_divide_a_string_into_groups_of_size_k/Solution.kt
@@ -1,14 +1,14 @@
 package leetcode.leetcode_2138_divide_a_string_into_groups_of_size_k
 
+import kotlin.math.ceil
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/divide-a-string-into-groups-of-size-k/description/?envType=daily-question&envId=2025-06-22
  *
- * TODO(Abhi) - To revisit
- *
- * Data Structure -
+ * Data Structure - [StringBuilder]
  * Algorithm -
  *
- * Difficulty -
+ * Difficulty - Easy
  *
  * Stats
  *
@@ -17,6 +17,25 @@ package leetcode.leetcode_2138_divide_a_string_into_groups_of_size_k
  *
  * Companies -
  */
+private fun divideString(s: String, k: Int, fill: Char): Array<String> {
+    val result = Array(ceil(s.length.toDouble() / k).toInt()) { "" }
+    var index = 0
+    while (index * k <= s.lastIndex) {
+        val current = StringBuilder()
+        var i = 0
+        while (i < k && (index * k) + i <= s.lastIndex) {
+            current.append(s[(index * k) + i])
+            i++
+        }
+        while (current.length < k) {
+            current.append(fill)
+        }
+        result[index] = current.toString()
+        index++
+    }
+    return result
+}
+
 private fun main() {
 
 }
