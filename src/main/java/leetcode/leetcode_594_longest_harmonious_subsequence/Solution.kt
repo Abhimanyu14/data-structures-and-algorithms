10a43acc017747348c4ce1c5d1f73d5d8f26c8f3
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-06-30 06:26:58 +0530

    leetcode

--- src/main/java/leetcode/leetcode_594_longest_harmonious_subsequence/Solution.kt
+++ src/main/java/leetcode/leetcode_594_longest_harmonious_subsequence/Solution.kt
@@ -1,22 +1,36 @@
 package leetcode.leetcode_594_longest_harmonious_subsequence
 
+import kotlin.math.max
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/longest-harmonious-subsequence/description/?envType=daily-question&envId=2025-06-30
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - [Map]
+ * Algorithm - Counting
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Easy
  *
  * Stats
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(N)
  *
- * Companies -
+ * Companies - Amazon, Apple, Google, Meta, Microsoft
  */
+private fun findLHS(nums: IntArray): Int {
+    var result = 0
+    val counter = mutableMapOf<Int, Int>()
+    for (num in nums) {
+        counter[num] = counter.getOrDefault(num, 0) + 1
+    }
+    for ((key, value1) in counter) {
+        counter[key + 1]?.let { value2 ->
+            result = max(result, value1 + value2)
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
