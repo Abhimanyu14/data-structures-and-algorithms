72c546fc359d6e94dfac068b5346ea63f9a3db60
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-07-14 06:29:30 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1290_convert_binary_number_in_a_linked_list_to_integer/Solution.kt
+++ src/main/java/leetcode/leetcode_1290_convert_binary_number_in_a_linked_list_to_integer/Solution.kt
@@ -1,22 +1,32 @@
 package leetcode.leetcode_1290_convert_binary_number_in_a_linked_list_to_integer
 
+import data_structures.linkedlist.ListNode
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/?envType=daily-question&envId=2025-07-14
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Linked List
+ * Algorithm - Iteration
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Easy
  *
  * Stats
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(1)
  *
- * Companies -
+ * Companies - Amazon, Google, Meta, Microsoft
  */
+private fun getDecimalValue(head: ListNode?): Int {
+    var result = 0
+    var current = head
+    while (current != null) {
+        result = (result * 2) + (current.`val`)
+        current = current.next
+    }
+    return result
+}
+
 private fun main() {
 
 }
