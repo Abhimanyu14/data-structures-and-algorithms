dd0312374da27e547c39fbc3251f2658c243bebc
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-07-15 22:22:53 +0530

    leetcode

--- src/main/java/leetcode/leetcode_3483_unique_3_digit_even_numbers/Solution.kt
+++ src/main/java/leetcode/leetcode_3483_unique_3_digit_even_numbers/Solution.kt
@@ -17,6 +17,26 @@ package leetcode.leetcode_3483_unique_3_digit_even_numbers
  *
  * Companies - Amazon, Google
  */
+private fun totalNumbers(digits: IntArray): Int {
+    val set = mutableSetOf<Int>()
+    for (i in digits.indices) {
+        if (digits[i] == 0) {
+            continue
+        }
+        for (j in digits.indices) {
+            if (i == j) {
+                continue
+            }
+            for (k in digits.indices) {
+                if (i != k && j != k && digits[k] % 2 == 0) {
+                    set.add(digits[i] * 100 + digits[j] * 10 + digits[k])
+                }
+            }
+        }
+    }
+    return set.size
+}
+
 private fun main() {
 
 }
