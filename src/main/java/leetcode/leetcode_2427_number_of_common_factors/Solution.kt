574ce0b04c4379710fe5bacf7e14cfdc526b9da0
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-07-26 10:32:56 +0530

    leetcode

--- src/main/java/leetcode/leetcode_2427_number_of_common_factors/Solution.kt
+++ src/main/java/leetcode/leetcode_2427_number_of_common_factors/Solution.kt
@@ -1,22 +1,34 @@
 package leetcode.leetcode_2427_number_of_common_factors
 
+import kotlin.math.min
+
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/number-of-common-factors/submissions/1710932570/?envType=company&envId=google&favoriteSlug=google-thirty-days
  *
  * TODO(Abhi) - To revisit
  *
  * Data Structure -
  * Algorithm -
  *
- * Difficulty -
+ * Difficulty - Easy
  *
  * Stats
  *
  * Time -
  * Space -
  *
- * Companies -
+ * Companies - Google, Meta
  */
+private fun commonFactors(a: Int, b: Int): Int {
+    var result = 0
+    for (i in 1..min(a, b)) {
+        if (a % i == 0 && b % i == 0) {
+            result++
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
