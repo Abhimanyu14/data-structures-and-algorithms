85f86ba4e4d4a14f910080bdb67a03e1f623d5bf
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-07-27 12:11:44 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1111_maximum_nesting_depth_of_two_valid_parentheses_strings/Solution.kt
+++ src/main/java/leetcode/leetcode_1111_maximum_nesting_depth_of_two_valid_parentheses_strings/Solution.kt
@@ -1,21 +1,21 @@
 package leetcode.leetcode_1111_maximum_nesting_depth_of_two_valid_parentheses_strings
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/maximum-nesting-depth-of-two-valid-parentheses-strings/description/?envType=company&envId=google&favoriteSlug=google-thirty-days
  *
  * TODO(Abhi) - To revisit
  *
  * Data Structure -
  * Algorithm -
  *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
  *
  * Time -
  * Space -
  *
- * Companies -
+ * Companies - Google
  */
 private fun main() {
 
--- src/main/java/leetcode/leetcode_2210_count_hills_and_valleys_in_an_array/Solution.kt
+++ src/main/java/leetcode/leetcode_2210_count_hills_and_valleys_in_an_array/Solution.kt
@@ -5,14 +5,71 @@ import kotlin.math.sign
 /**
  * leetcode - https://leetcode.com/problems/count-hills-and-valleys-in-an-array/
  *
- * Using single pass
- * [Int.sign] => return sign of an [Int] => -1, 0, or 1
+ * Data Structure - NA
+ * Algorithm - Iteration
+ *
+ * Difficulty - Easy
  *
  * Stats
  * Runtime: 165 ms, faster than 50.00%
  * Memory Usage: 34.1 MB, less than 70.00%
+ *
+ * Time - O(N)
+ * Space - O(1)
+ *
+ * Companies - Google, Meta, Microsoft
  */
 private fun countHillValley(nums: IntArray): Int {
+    var i = 1
+    while (i <= nums.lastIndex && nums[i - 1] == nums[i]) {
+        i++
+    }
+    if (i > nums.lastIndex) {
+        return 0
+    }
+
+    var result = 0
+    fun getSlope(x: Int, y: Int): Int {
+        return if (x > y) {
+            -1
+        } else if (x == y) {
+            0
+        } else {
+            1
+        }
+    }
+
+    var slope = getSlope(nums[i - 1], nums[i])
+    while (i <= nums.lastIndex) {
+        val current = getSlope(nums[i - 1], nums[i])
+        if (current != 0 && current != slope) {
+            result++
+            slope = current
+        }
+        i++
+    }
+    return result
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/count-hills-and-valleys-in-an-array/
+ *
+ * Data Structure -
+ * Algorithm - Iteration
+ * [Int.sign] => return sign of an [Int] => -1, 0, or 1
+ *
+ * Difficulty - Easy
+ *
+ * Stats
+ * Runtime: 165 ms, faster than 50.00%
+ * Memory Usage: 34.1 MB, less than 70.00%
+ *
+ * Time - O(N)
+ * Space - O(1)
+ *
+ * Companies - Google, Meta, Microsoft
+ */
+private fun countHillValleyUsingSign(nums: IntArray): Int {
     var result = 0
     var slope = 0
     var i = 1
