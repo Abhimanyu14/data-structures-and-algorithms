4c4c82b45bc04f7faf6eb3c3cc2e709a39f457e9
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-07-28 10:46:18 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1111_maximum_nesting_depth_of_two_valid_parentheses_strings/Solution.kt
+++ src/main/java/leetcode/leetcode_1111_maximum_nesting_depth_of_two_valid_parentheses_strings/Solution.kt
@@ -5,18 +5,40 @@ package leetcode.leetcode_1111_maximum_nesting_depth_of_two_valid_parentheses_st
  *
  * TODO(Abhi) - To revisit
  *
- * Data Structure -
- * Algorithm -
+ * Data Structure - [IntArray]
+ * Algorithm - Iteration
  *
  * Difficulty - Medium
  *
  * Stats
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(1)
  *
  * Companies - Google
  */
+private fun maxDepthAfterSplit(seq: String): IntArray {
+    val result = IntArray(seq.length)
+    var current = 0
+    var openCount = 0
+    for (i in seq.indices) {
+        if (seq[i] == '(') {
+            current++
+            if (current % 2 == 0) {
+                result[i] = 1
+                openCount++
+            }
+        } else {
+            if (openCount > 0 && current % 2 == 0) {
+                result[i] = 1
+                openCount--
+            }
+            current--
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
