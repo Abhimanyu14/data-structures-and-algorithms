6e1ba87692fc913e6334c08eb9886012fce9001d
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-12-19 00:52:28 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1502_can_make_arithmetic_progression_from_sequence/Solution.kt
+++ src/main/java/leetcode/leetcode_1502_can_make_arithmetic_progression_from_sequence/Solution.kt
@@ -1,22 +1,71 @@
 package leetcode.leetcode_1502_can_make_arithmetic_progression_from_sequence
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/?envType=company&envId=google&favoriteSlug=google-thirty-days
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - Array, Set
+ * Algorithm - Iteration, Simulation
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Easy
  *
  * Stats
  *
- * Time -
- * Space -
+ * Time - O(N)
+ * Space - O(N)
  *
- * Companies -
+ * Companies - Google, Meta, Amazon
  */
-private fun main() {
+private fun canMakeArithmeticProgression(arr: IntArray): Boolean {
+    var maxValue = arr[0]
+    var minValue = arr[0]
+    for (num in arr) {
+        maxValue = maxOf(maxValue, num)
+        minValue = minOf(minValue, num)
+    }
+    val maxMinDiff = (maxValue - minValue)
+    if (maxMinDiff % (arr.size - 1) != 0) {
+        return false
+    }
+    if (maxMinDiff == 0) {
+        return true
+    }
+    val diff = maxMinDiff / (arr.size - 1)
+    val set = mutableSetOf<Int>()
+    for (i in minValue..maxValue step diff) {
+        set.add(i)
+    }
+    for (num in arr) {
+        if (set.contains(num)) {
+            set.remove(num)
+        } else {
+            return false
+        }
+    }
+    return true
+}
 
+/**
+ * leetcode - https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/?envType=company&envId=google&favoriteSlug=google-thirty-days
+ *
+ * Data Structure - Array
+ * Algorithm - Sorting
+ *
+ * Difficulty - Easy
+ *
+ * Stats
+ *
+ * Time - O(N * log(N))
+ * Space - O(N)
+ *
+ * Companies - Google, Meta, Amazon
+ */
+private fun canMakeArithmeticProgressionUsingSorting(arr: IntArray): Boolean {
+    val sorted = arr.sorted()
+    val diff = sorted[1] - sorted[0]
+    for (i in 2 until sorted.size) {
+        if (sorted[i] - sorted[i - 1] != diff) {
+            return false
+        }
+    }
+    return true
 }
--- src/main/java/leetcode/leetcode_811_subdomain_visit_count/Solution.kt
+++ src/main/java/leetcode/leetcode_811_subdomain_visit_count/Solution.kt
@@ -1,22 +1,34 @@
 package leetcode.leetcode_811_subdomain_visit_count
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/subdomain-visit-count/?envType=company&envId=google&favoriteSlug=google-thirty-days
  *
- * TODO(Abhi) - To revisit
+ * Data Structure - String, List, Map
+ * Algorithm - Iteration
  *
- * Data Structure -
- * Algorithm -
- *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
  *
  * Time -
  * Space -
  *
- * Companies -
+ * Companies - Google, Microsoft
  */
-private fun main() {
-
+private fun subdomainVisits(cpdomains: Array<String>): List<String> {
+    val map = mutableMapOf<String, Int>()
+    for (cpdomain in cpdomains) {
+        val (domainCountString, domainsString) = cpdomain.split(" ")
+        val domainCount = domainCountString.toInt()
+        val domains = domainsString.split(".")
+        var currentDomain = domains.last()
+        map[currentDomain] = map.getOrDefault(currentDomain, 0) + domainCount
+        for (i in domains.size - 2 downTo 0) {
+            currentDomain = "${domains[i]}.$currentDomain"
+            map[currentDomain] = map.getOrDefault(currentDomain, 0) + domainCount
+        }
+    }
+    return map.map {
+        "${it.value} ${it.key}"
+    }
 }
