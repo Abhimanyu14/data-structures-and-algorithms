320beedc41b44567bae0219ea695f90730eafbca
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-12-25 18:33:17 +0530

    leetcode

--- src/main/java/leetcode/leetcode_3075_maximize_happiness_of_selected_children/Solution.kt
+++ src/main/java/leetcode/leetcode_3075_maximize_happiness_of_selected_children/Solution.kt
@@ -3,24 +3,48 @@ package leetcode.leetcode_3075_maximize_happiness_of_selected_children
 import java.util.PriorityQueue
 import kotlin.math.max
 
-
 /**
  * leetcode - https://leetcode.com/problems/maximize-happiness-of-selected-children/
  *
  * TODO(Abhi) - To revisit
  *
- * Using
+ * Data Structure - Array
+ * Algorithm - Sorting
  *
  * Difficulty - Medium
  *
  * Stats
- * Runtime: 818 ms, faster than 16.87%
- * Memory Usage: 80.8 MB, less than 16.87%
  *
- * Time -
- * Space -
+ * Time - O(N * log(N))
+ * Space - O(N)
+ *
+ * Companies - Amazon
  */
 private fun maximumHappinessSum(happiness: IntArray, k: Int): Long {
+    var result = 0L
+    val sorted = happiness.sortedArrayDescending()
+    for (i in 0..<k) {
+        result += max(0, sorted[i] - i)
+    }
+    return result
+}
+
+/**
+ * leetcode - https://leetcode.com/problems/maximize-happiness-of-selected-children/
+ *
+ * Data Structure - PriorityQueue
+ * Algorithm - Heapify
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ *
+ * Time - O(N * log(N) + K * log(K))
+ * Space - O(N)
+ *
+ * Companies - Amazon
+ */
+private fun maximumHappinessSumUsingPriorityQueue(happiness: IntArray, k: Int): Long {
     var result = 0L
     val priorityQueue = PriorityQueue<Int>(k) { a, b ->
         b - a
