7e3bab09c22b93886782cf7c65c472f873dbf930
Abhimanyu14 <abhimanyu.n14@gmail.com>
2026-04-24 17:59:04 +0530

    leetcode

--- src/main/java/leetcode/leetcode_1967_number_of_strings_that_appear_as_substrings_in_word/Solution.kt
+++ src/main/java/leetcode/leetcode_1967_number_of_strings_that_appear_as_substrings_in_word/Solution.kt
@@ -1,22 +1,32 @@
 package leetcode.leetcode_1967_number_of_strings_that_appear_as_substrings_in_word
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/?envType=company&envId=uber&favoriteSlug=uber-all
  *
- * TODO(Abhi) - To revisit
+ * TODO(Abhi) - To revisit, try using Trie / K-D Tree
  *
- * Data Structure -
- * Algorithm -
+ * Data Structure - Strings
+ * Algorithm - Iteration
  *
- * Difficulty -
+ * Difficulty - Easy
  *
  * Stats
  *
- * Time -
- * Space -
+ * Time - O(M * N)
+ * Space - O(1)
  *
  * Companies -
  */
+private fun numOfStrings(patterns: Array<String>, word: String): Int {
+    var count = 0
+    for (pattern in patterns) {
+        if (word.contains(pattern)) {
+            count++
+        }
+    }
+    return count
+}
+
 private fun main() {
 
 }
---
+++ src/main/java/leetcode/leetcode_3907_count_smaller_elements_with_opposite_parity/Solution.kt
@@ -0,0 +1,22 @@
+package leetcode.leetcode_3907_count_smaller_elements_with_opposite_parity
+
+/**
+ * leetcode - https://leetcode.com/problems/count-smaller-elements-with-opposite-parity/
+ *
+ * TODO(Abhi) - To revisit
+ *
+ * Data Structure -
+ * Algorithm -
+ *
+ * Difficulty - Medium
+ *
+ * Stats
+ *
+ * Time -
+ * Space -
+ *
+ * Companies -
+ */
+private fun main() {
+
+}
