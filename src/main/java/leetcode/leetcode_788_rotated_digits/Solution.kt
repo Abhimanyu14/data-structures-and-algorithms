9e4c5062c8aae4d2270c5f9c7ddc9e03c7e34657
Abhimanyu14 <abhimanyu.n14@gmail.com>
2026-05-02 12:14:59 +0530

    leetcode

--- src/main/java/leetcode/leetcode_788_rotated_digits/Solution.kt
+++ src/main/java/leetcode/leetcode_788_rotated_digits/Solution.kt
@@ -1,14 +1,14 @@
 package leetcode.leetcode_788_rotated_digits
 
 /**
- * leetcode -
+ * leetcode - https://leetcode.com/problems/rotated-digits/description/?envType=daily-question&envId=2026-05-02
  *
  * TODO(Abhi) - To revisit
  *
  * Data Structure -
- * Algorithm -
+ * Algorithm - Simulation / Enumeration
  *
- * Difficulty -
+ * Difficulty - Medium
  *
  * Stats
  *
@@ -17,6 +17,31 @@ package leetcode.leetcode_788_rotated_digits
  *
  * Companies -
  */
+private fun rotatedDigits(n: Int): Int {
+    fun isValidNumber(x: Int): Boolean {
+        var current = x
+        var flag = false
+        while (current != 0) {
+            if (current % 10 == 3 || current % 10 == 4 || current % 10 == 7) {
+                return false
+            }
+            if (current % 10 == 2 || current % 10 == 5 || current % 10 == 6 || current % 10 == 9) {
+                flag = true
+            }
+            current /= 10
+        }
+        return flag
+    }
+
+    var result = 0
+    for (i in 1..n) {
+        if (isValidNumber(i)) {
+            result++
+        }
+    }
+    return result
+}
+
 private fun main() {
 
 }
